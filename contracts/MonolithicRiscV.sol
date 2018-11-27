fffdd66b97151e896a18514d696f795b91839ac9
Felipe Argento <felipe@cartesi.io>
2018-11-27 15:31:27 +0800

    check to iflags_h, implementing raise_interrupt(), get_pending_irq_mask and ilog2 functions

--- contracts/MonolithicRiscV.sol
+++ contracts/MonolithicRiscV.sol
@@ -19,22 +19,31 @@ contract MonolithicRiscV {
   uint256 mmIndex;
 
   function step(address _memoryManagerAddress) returns (interpreter_status){
-    //instantiate mmInterface to correct address
-    //Read iflags_H to find out if machine is halted
+    uint64 pc = 0;
+    uint32 insn = 0;
 
+    //TO-DO: set mmInterface to correct address
+    mm = mmInterface(_memoryManagerAddress);
+    //TO-DO: Check byte order -> riscv is little endian/ solidity is big endian
+
+    //H -> least significant bit of iflags
+    if( (uint64(mm.read(mmIndex, ShadowAddresses.get_iflags())) & 1) != 0){
+      //machine is halted
+      return interpreter_status.success;
+    }
     //Raise the highest priority interrupt
-      //get_pending_irq_mask
+    raise_interrupt_if_any();
 
-//    if(fetch_insn() == fetch_status.success){
-//      if(execute_insn == execute_status.retired){
-//        decodes instruction until it finds the definitive one
-//        begin auipc
-//          write_register(rd, pc + insn_U_get_imm
-//          advance_to_next_insn
-//          write_pc = pc + 4
-//        end auipc
-//      }
-//    }
+    if(fetch_insn() == fetch_status.success){
+      if(true/*execute_insn == execute_status.retired*/){
+        //decodes instruction until it finds the definitive one
+        //begin auipc
+          //write_register(rd, pc + insn_U_get_imm
+          //advance_to_next_insn
+            //write_pc = pc + 4
+          //end auipc
+      }
+    }
     //read_minstret
     //write_minsret + 1
 
@@ -48,11 +57,57 @@ contract MonolithicRiscV {
 
     //translate_virtual_address();
 
+    //how to find paddr?? Some cases paddr == pc?
     //find_pma_entry
     //if pma is memory:
       //read_memory
     //end fetch
 
+  }
+  function get_pending_irq_mask() returns (uint32){
+    uint64 mip = uint64(mm.read(mmIndex, ShadowAddresses.get_mip()));
+    uint64 mie = uint64(mm.read(mmIndex, ShadowAddresses.get_mie()));
+
+    uint32 pending_ints = uint32(mip & mie);
+    if(pending_ints == 0){
+      return 0;
+    }
+    uint64 mstatus = 0;
+    uint32 enabled_ints = 0;
+    //TO-DO: check shift + mask
+    //TO-DO: Use bitmanipulation library for arithmetic shift
+    int priv = (uint64(mm.read(mmIndex, ShadowAddresses.get_iflags())) >> 2) & 3;
+    if(priv == PRVLevelsConstants.get_PRV_M()) {
+      mstatus = uint64(mm.read(mmIndex, ShadowAddresses.get_mstatus()));
+      if((mstatus & MstatusConstants.get_MSTATUS_MIE()) != 0){
+        enabled_ints = uint32(~uint64(mm.read(mmIndex, ShadowAddresses.get_mideleg())));
+      }
+    }else if(priv == PRVLevelsConstants.get_PRV_S()){
+      mstatus = uint64(mm.read(mmIndex, ShadowAddresses.get_mstatus()));
+      uint64 mideleg = uint64(mm.read(mmIndex, ShadowAddresses.get_mideleg()));
+      enabled_ints = uint32(~mideleg);
+      if((mstatus & MstatusConstants.get_MSTATUS_SIE()) != 0){
+        //TO-DO: make sure this is the correct cast
+        enabled_ints = enabled_ints | uint32(mideleg);
+      }
+    }else{
+      //TO-DO: Should I require iflags_PRV == PRV_U?
+      //require(priv == PRVLevelsConstants.get_PRV_U());
+      enabled_ints = uint32(-1);
+    }
+    return pending_ints & enabled_ints;
+  }
+  function ilog2(uint32 v){
+    //cpp emulator code:
+    //return 31 - __builtin_clz(v)
+
+    //TO-DO: What to do if v == 0?
+    uint leading = 32;
+    while(v != 0){
+      v = v >> 1;
+      leading--;
+    }
+    return 31 - leading;
   }
   //enums
   enum fetch_status {
