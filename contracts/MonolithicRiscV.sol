9f497bfd3aa746c737eb3f5306297a04437e705f
Felipe Argento <felipe@cartesi.io>
2018-12-12 15:05:22 +0800

    Improving documentation
    Significantly increased documentation, adding in depth explanation and
    clear directions, in the form of references, for finding the resources
    pertaining to that specific snippet of code. The references include the
    Core of Cartesi techpaper, the riscv spec and riscv privileged, all
    easily available online.

--- contracts/MonolithicRiscV.sol
+++ contracts/MonolithicRiscV.sol
@@ -37,7 +37,10 @@ contract MonolithicRiscV {
   int pte_size_log2;
    int vpn_bits;
   //structs
-  //pma
+
+   // PMA stands for Physical Memory Attributes - it is defined as two 64 bit words
+  // The first word defines start and flags and the second defines length.
+  // Reference: The Core of Cartesi, v1.02 - figure 2.
   struct PMAEntry{
     uint64 start;
     uint64 length;
@@ -53,7 +56,10 @@ contract MonolithicRiscV {
     mm = mmInterface(_memoryManagerAddress);
     //TO-DO: Check byte order -> riscv is little endian/ solidity is big endian
 
-    //H -> least significant bit of iflags
+    // Read iflags register and check its H flag, to see if machine is halted.
+    // If machine if it is halted - nothing else to do. H flag is stored on the
+    // least signficant bit on iflags register.
+    // Reference: The Core of Cartesi, v1.02 - figure 1.
     if( (uint64(mm.read(mmIndex, ShadowAddresses.get_iflags())) & 1) != 0){
       //machine is halted
       return interpreter_status.success;
@@ -93,9 +99,14 @@ contract MonolithicRiscV {
       return fetch_status.exception;
     }
 
+    // Finds the range in memory in which the physical address is located
     find_pma_entry(paddr);
 
-    if(pma_get_istart_M() || !pma_get_istart_X()){
+    // M flag defines if the pma range is in memory 
+    // X flag defines if the pma is executable
+    // If the pma is not memory or not executable - this is a pma violation
+    // Reference: The Core of Cartesi, v1.02 - section 3.2 the board - page 5.
+    if(!pma_get_istart_M() || !pma_get_istart_X()){
       //raise_exception(CAUSE_FETCH_FAULT)
       return fetch_status.exception;
     }
@@ -106,10 +117,20 @@ contract MonolithicRiscV {
     return fetch_status.success;
   }
 
-  //TO-DO: Understand this code properly
+  // Finds the physical address associated to the virtual address (vaddr).
+  // Walks the page table until it finds a valid one. Returns a bool if the physical
+  // address was succesfully found along with the address. Returns false and zer0
+  // if something went wrong.
+
+  // Virtual Address Translation proccess is defined, step by step on the following Reference:
+  // Reference: riscv-priv-spec-1.10.pdf - Section 4.3.2, page 62.
   function translate_virtual_address(uint64 vaddr, int xwr_shift) returns(bool, uint64){
     //TO-DO: check shift + mask
     //TO-DO: use bitmanipulation right shift
+
+    // Reads privilege level on iflags register. The privilege level is located
+    // on bits 2 and 3.
+    // Reference: The Core of Cartesi, v1.02 - figure 1.
     priv = (uint64(mm.read(mmIndex, ShadowAddresses.get_iflags())) >> 2) & 3;
     //read_mstatus
     mstatus = uint64(mm.read(mmIndex, ShadowAddresses.get_mstatus()));
@@ -120,16 +141,24 @@ contract MonolithicRiscV {
     if((mstatus & RiscVConstants.MSTATUS_MPRV() != 0) && (xwr_shift != RiscVConstants.PTE_XWR_CODE_SHIFT())){
       priv = (mstatus >> RiscVConstants.MSTATUS_MPP_SHIFT()) & 3;
     }
-
+    // Physical memory is mediated by Machine-mode so, if privilege is M-mode it 
+    // does not use virtual Memory
+    // Reference: riscv-priv-spec-1.7.pdf - Section 3.3, page 32.
     if(priv == RiscVConstants.PRV_M()){
       return(true, vaddr);
     }
 
+    // SATP - Supervisor Address Translation and Protection Register
+    // Holds MODE, Physical page number (PPN) and address space identifier (ASID)
+    // MODE is located on bits 60 to 63 for RV64.
+    // Reference: riscv-priv-spec-1.10.pdf - Section 4.1.12, page 56.
     satp = uint64(mm.read(mmIndex, ShadowAddresses.get_satp()));
+
     // In RV64, mode can be
     //   0: Bare: No translation or protection
     //   8: sv39: Page-based 39-bit virtual addressing
     //   9: sv48: Page-based 48-bit virtual addressing
+    // Reference: riscv-priv-spec-1.10.pdf - Table 4.3, page 57.
     mode = (satp >> 60) & 0xf;
     if(mode == 0){
       return(true, vaddr);
@@ -140,25 +169,29 @@ contract MonolithicRiscV {
 
     // Page table hierarchy of sv39 has 3 levels, and sv48 has 4 levels
     levels = mode - 8 + 3;
-    // The least significant 12 bits of vaddr are the page offset
+    // Page offset are bits located from 0 to 11.
     // Then come levels virtual page numbers (VPN)
     // The rest of vaddr must be filled with copies of the
     // most significant bit in VPN[levels]
     // Hence, the use of arithmetic shifts here
+    // Reference: riscv-priv-spec-1.10.pdf - Figure 4.16, page 63.
 
     //TO-DO: Use bitmanipulation library for arithmetic shift
     vaddr_shift = RiscVConstants.XLEN() - (RiscVConstants.PG_SHIFT() + levels * 9);
     if(((int64(vaddr) << vaddr_shift) >> vaddr_shift) != int64(vaddr)){
       return(false, 0);
     }
-    // The least significant 44 bits of satp contain the physical page number for the root page table
+    // The least significant 44 bits of satp contain the physical page number
+    // for the root page table
+    // Reference: riscv-priv-spec-1.10.pdf - Figure 4.12, page 57.
     int satp_ppn_bits = 44;
     // Initialize pte_addr with the base address for the root page table
     pte_addr = (satp & ((uint64(1) << satp_ppn_bits) -1)) << RiscVConstants.PG_SHIFT();
     // All page table entries have 8 bytes
-    pte_size_log2 = 3;
     // Each page table has 4k/pte_size entries
     // To index all entries, we need vpn_bits
+    // Reference: riscv-priv-spec-1.10.pdf - Section 4.4.1, page 63.
+    pte_size_log2 = 3;
     vpn_bits = 12 - pte_size_log2;
     uint64 vpn_mask = uint64((1 << vpn_bits) - 1);
 
@@ -170,10 +203,12 @@ contract MonolithicRiscV {
       pte_addr += vpn << pte_size_log2;
       //Read page table entry from physical memory
       uint64 pte = 0;
-//===//TO-DO: Implement read_ram_uint64(a, pte_addr, &pte)
-     // if(!read_ram_uint64(pte_addr)){
-     //   return(false, 0);
-//===// }
+
+      //TO-DO: Implement read_ram_uint64(a, pte_addr, &pte)
+      // if(!read_ram_uint64(pte_addr)){
+      //   return(false, 0);
+      // }
+
       // The OS can mark page table entries as invalid,
       // but these entries shouldn't be reached during page lookups
       //TO-DO: check if condition
@@ -183,6 +218,8 @@ contract MonolithicRiscV {
       // Clear all flags in least significant bits, then shift back to multiple of page size to form physical address
       uint64 ppn = (pte >> 10) << RiscVConstants.PG_SHIFT();
       // Obtain X, W, R protection bits
+      // X, W, R bits are located on bits 1 to 3 on physical address
+      // Reference: riscv-priv-spec-1.10.pdf - Figure 4.18, page 63.
       int xwr = (pte >> 1) & 7;
       // xwr !=0 means we are done walking the page tables
       if(xwr !=0){
@@ -213,7 +250,7 @@ contract MonolithicRiscV {
           return (false, 0);
         }
         // Check page, megapage, and gigapage alignment
-        uint64 vaddr_mask = (uint64(1) << vaddr_shift) - 1;
+        vaddr_mask = (uint64(1) << vaddr_shift) - 1;
         if(ppn & vaddr_mask != 0){
           return (false, 0);
         }
@@ -241,10 +278,18 @@ contract MonolithicRiscV {
   //TO-DO: there is probably gonna be a pma_entry array on storage, so we will have
   // to adjust this code.
   function find_pma_entry(uint64 paddr){
+
     //Hard coded ram address starts at 0x800
+    // In total there are 32 PMAs from processor shadow to Flash disk 7.
+    // PMA 0 - describes RAM and is hardcoded to address 0x800
+    // PMA 16 - 23 describe flash devices 0-7
+    // RAM start field is hardcoded to 0x800
+    // Reference: The Core of Cartesi, v1.02 - Table 3.
     uint64 pmaAddress = 0x800;
     bool foundPma;
-    uint64 lastPma = 23;
+
+    //TO-DO: Check lastPma - this is probably wrong.
+    uint64 lastPma = 62; // 0 - 31 * 2 words
 
     for(uint64 i = 0; i < lastPma; i+=2){
       uint64 start = uint64(mm.read(mmIndex, pmaAddress + (i*8)));
@@ -268,45 +313,64 @@ contract MonolithicRiscV {
     }
   }
 
+  // Machine Interrupt Registers: mip and mie.
+  // mip register contains information on pending interrupts.
+  // mie register contains the interrupt enabled bits.
+  // Reference: riscv-privileged-v1.10 - section 3.1.14 - page 28.
   function get_pending_irq_mask() returns (uint32){
     uint64 mip = uint64(mm.read(mmIndex, ShadowAddresses.get_mip()));
     uint64 mie = uint64(mm.read(mmIndex, ShadowAddresses.get_mie()));
 
     uint32 pending_ints = uint32(mip & mie);
+    // if there are no pending interrupts, return 0.
     if(pending_ints == 0){
       return 0;
     }
-    uint64 mstatus = 0;
+    mstatus = 0;
     uint32 enabled_ints = 0;
     //TO-DO: check shift + mask
     //TO-DO: Use bitmanipulation library for arithmetic shift
+
+    // Read privilege level on iflags register.
+    // The privilege level is represented by bits 2 and 3 on iflags register.
+    // Reference: The Core of Cartesi, v1.02 - figure 1.
     priv = (uint64(mm.read(mmIndex, ShadowAddresses.get_iflags())) >> 2) & 3;
     if(priv == RiscVConstants.PRV_M()) {
+      // MSTATUS is the Machine Status Register - it controls the current
+      // operating state. The MIE is an interrupt-enable bit for machine mode.
+      // MIE for 64bit is stored on location 3 - according to:
+      // Reference: riscv-privileged-v1.10 - figure 3.7 - page 20.
       mstatus = uint64(mm.read(mmIndex, ShadowAddresses.get_mstatus()));
       if((mstatus & RiscVConstants.MSTATUS_MIE()) != 0){
         enabled_ints = uint32(~uint64(mm.read(mmIndex, ShadowAddresses.get_mideleg())));
       }
     }else if(priv == RiscVConstants.PRV_S()){
       mstatus = uint64(mm.read(mmIndex, ShadowAddresses.get_mstatus()));
+      // MIDELEG: Machine trap delegation register
+      // mideleg defines if a interrupt can be proccessed by a lower privilege
+      // level. If mideleg bit is set, the trap will delegated to the S-Mode.
+      // Reference: riscv-privileged-v1.10 - Section 3.1.13 - page 27.
       uint64 mideleg = uint64(mm.read(mmIndex, ShadowAddresses.get_mideleg()));
       enabled_ints = uint32(~mideleg);
+
+
+      // SIE: is the register contaning interrupt enabled bits for supervisor mode.
+      // It is located on the first bit of mstatus register (RV64).
+      // Reference: riscv-privileged-v1.10 - figure 3.7 - page 20.
       if((mstatus & RiscVConstants.MSTATUS_SIE()) != 0){
         //TO-DO: make sure this is the correct cast
         enabled_ints = enabled_ints | uint32(mideleg);
       }
     }else{
-      //TO-DO: Should I require iflags_PRV == PRV_U?
-      //require(priv == PRVLevelsConstants.get_PRV_U());
       enabled_ints = uint32(-1);
     }
     return pending_ints & enabled_ints;
   }
-
+  //TO-DO: optmize log2 function
   function ilog2(uint32 v) returns(uint64){
     //cpp emulator code:
     //return 31 - __builtin_clz(v)
 
-    //TO-DO: What to do if v == 0?
     uint leading = 32;
     while(v != 0){
       v = v >> 1;
@@ -316,11 +380,18 @@ contract MonolithicRiscV {
   }
 
   //pma functions
+
+  // M bit defines if the range is memory
+  // The flag is pma_entry start's word first bit
+  // Reference: The Core of Cartesi, v1.02 - figure 2.
   function pma_get_istart_M() returns(bool){
     //M is pma_entry fisrt bit
     return pma_entry.start & 1 == 1;
   }
 
+  // X bit defines if the range is executable
+  // The flag is pma_entry start's word on position 5.
+  // Reference: The Core of Cartesi, v1.02 - figure 2.
   function pma_get_istart_X() returns(bool){
     //X is pma_entry sixth bit (index 5)
     return (pma_entry.start >> 5) & 1 == 1;
