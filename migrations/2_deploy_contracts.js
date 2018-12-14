ffd071062340fba86e4d39bfdfcfccf77a777f89
felipeargento <felipeargento@gmail.com>
2018-12-14 18:53:26 +0800

    Added contracts and libraries to migrate
    In order to set up the testing environment it is necessary to let
    truffle know where the contracts and libraries are at and what is their
    connection with eachother. Adding that to the migration files, makes it
    possible to link and deploy automatically the contracts on their last
    version.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,3 +1,33 @@
+//Libraries
+var RiscVDecoder = artifacts.require("./RiscVDecoder.sol");
+var ShadowAddresses = artifacts.require("./ShadowAddresses.sol");
+var RiscVConstants = artifacts.require("./RiscVConstants.sol");
+var BranchInstructions = artifacts.require("./RiscVInstructions/BranchInstructions.sol");
+var ArithmeticInstructions = artifacts.require("./RiscVInstructions/ArithmeticInstructions.sol");
+
+//Contracts
+var MMInstantiator = artifacts.require("./MMInstantiator.sol");
+var MonolithicRiscV = artifacts.require("./MonolithicRiscV.sol");
+
 
 module.exports = function(deployer) {
+  //Deploy libraries      
+  deployer.deploy(ShadowAddresses);
+  deployer.deploy(RiscVConstants);
+  deployer.deploy(BranchInstructions);
+  deployer.deploy(ArithmeticInstructions);
+      
+  //Link Instruction libraries to Decoder 
+  deployer.link(BranchInstructions, RiscVDecoder);
+  deployer.link(ArithmeticInstructions, RiscVDecoder);
+        
+  deployer.deploy(RiscVDecoder);
+
+  //Link all librarie to Monolithic
+  deployer.link(RiscVDecoder, MonolithicRiscV);
+  deployer.link(ShadowAddresses, MonolithicRiscV);
+  deployer.link(RiscVConstants, MonolithicRiscV);
+        
+  deployer.deploy(MMInstantiator);
+  deployer.deploy(MonolithicRiscV);
 };
