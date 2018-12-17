1d3285076f32e940d0e6317139d2d6a94553216e
felipeargento <felipeargento@gmail.com>
2018-12-17 17:16:55 +0800

    Changed gasLimit for develop on truffle.js
    Manually set block gas limit to 4600000 on truffle.js so that it was
    possible to deploy monolithic riscv. This will likely not be a problem
    on main net cause the contract will be modularized and split between
    various libraries.

--- truffle.js
+++ truffle.js
@@ -3,7 +3,8 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+      gas: 4600000
     },
     solc: {
      optimizer: { // Turning on compiler optimization that removes some local variables during compilation
