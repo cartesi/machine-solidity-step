d5fbd0d9b60b133a0477493f0ceb8dcc6aafe2a4
Felipe Argento <felipe@cartesi.io>
2018-12-28 01:38:22 +0800

    Correcting reads on monolithictest
    Changed all read functions from ProveWrite to ProveRead and removed the
    parameter. Also increased gas sent to step function.

--- test/monolithictest.js
+++ test/monolithictest.js
@@ -12,6 +12,7 @@ var MMInstantiator = artifacts.require("./MMInstantiator.sol");
 contract('MonolithicRiscV', function(accounts){
   describe('Checking functionalities', async function() {
     let mmAddress;
+    let index;
 
     it('Writing to MM manager', async function() {
       let initialHash = 0;
@@ -32,53 +33,53 @@ contract('MonolithicRiscV', function(accounts){
       index = 0;
 
       //Write to iflags
-      response = await mm.proveWrite(index, 0x1d0, 0, 12, [], {
+      response = await mm.proveRead(index, 0x1d0, 12, [], {
         from: accounts[0],
         gas: 2000000
       });
 
       //Write to mip
-      response = await mm.proveWrite(index, 0x170, 0, 0, [], {
+      response = await mm.proveRead(index, 0x170, 0, [], {
         from: accounts[0],
         gas: 2000000
       });
       //Write to mie
-      response = await mm.proveWrite(index, 0x168, 0, 0, [], {
+      response = await mm.proveRead(index, 0x168, 0, [], {
         from: accounts[0],
         gas: 2000000
       });
       //Write to pc
-      response = await mm.proveWrite(index, 0x100, 0, 4096, [], {
+      response = await mm.proveRead(index, 0x100, 4096, [], {
         from: accounts[0],
         gas: 2000000
       });
       //Write to mstatus
-      response = await mm.proveWrite(index, 0x130, 0, 42949672960, [], {
+      response = await mm.proveRead(index, 0x130, 42949672960, [], {
         from: accounts[0],
         gas: 2000000
       });
       //Write to pma@800
-      response = await mm.proveWrite(index, 0x800, 0, 2147483649, [], {
+      response = await mm.proveRead(index, 0x800, 2147483649, [], {
         from: accounts[0],
         gas: 2000000
       });
       //Write to pma@808
-      response = await mm.proveWrite(index, 0x808, 0, 1048576, [], {
+      response = await mm.proveRead(index, 0x808, 1048576, [], {
         from: accounts[0],
         gas: 2000000
       });
       //Write to pma@810
-      response = await mm.proveWrite(index, 0x810, 0, 4097, [], {
+      response = await mm.proveRead(index, 0x810, 4097, [], {
         from: accounts[0],
         gas: 2000000
       });
       //Write to pma@818
-      response = await mm.proveWrite(index, 0x818, 0, 61440, [], {
+      response = await mm.proveRead(index, 0x818, 61440, [], {
         from: accounts[0],
         gas: 2000000
       });
       //Write to memory
-      response = await mm.proveWrite(index, 0x1000, 0, 162836104410563223, [], {
+      response = await mm.proveRead(index, 0x1000, 162836104410563223, [], {
         from: accounts[0],
         gas: 2000000
       });
@@ -87,7 +88,11 @@ contract('MonolithicRiscV', function(accounts){
       let riscV = await MonolithicRiscV.new({
         from: accounts[0], gas: 4600000
       });
-
+      response = await riscV.step(index, mmAddress, {
+        from: accounts[1],
+        gas: 4600000
+      });
+      expect(4).to.equal(0);
     });
   });
 });
