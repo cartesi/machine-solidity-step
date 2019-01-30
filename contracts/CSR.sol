9efb719676ea89dce267b2ab368a5d4de224e27f
felipeargento <felipeargento@gmail.com>
2019-01-30 16:48:05 +0800

    Added CSR rdcounteren() and csr_priv()
    Also included call to CSR.csr_read on Execute.sol library.

--- contracts/CSR.sol
+++ contracts/CSR.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.5.0;
 
 import "../contracts/MemoryInteractor.sol";
+import "../contracts/RiscVConstants.sol";
 
 library CSR {
-  
+
   //CSR addresses
   uint32 constant ustatus = 0x000;
   uint32 constant uie = 0x004;
@@ -73,73 +74,75 @@ library CSR {
     // Attemps to access a CSR without appropriate privilege level raises a
     // illegal instruction exception.
     // Reference: riscv-privileged-v1.10 - section 2.1 - page 7.
-    if(csr_priv(csr_address) > mi.read_iflags_PRV(mmIndex)){
+    if(csr_priv(csr_addr) > mi.read_iflags_PRV(mmIndex)){
       return(false, 0);
     }
-    if(csraddr == ucycle) {
-      return read_csr_cycle();
-    }else if(csraddr == uinstret){
-      return read_csr_instret(a, csraddr, status);
-    }else if(csraddr == utime){
-      return read_csr_time(a, csraddr, status);
-    }else if(csraddr == sstatus){
-      return read_csr_sstatus(a, status);
-    }else if(csraddr == sie){
-      return read_csr_sie(a, status);
-    }else if(csraddr == stvec){
-      return read_csr_stvec(a, status);
-    }else if(csraddr == scounteren){
-      return read_csr_scounteren(a, status);
-    }else if(csraddr == sscratch){
-      return read_csr_sscratch(a, status);
-    }else if(csraddr == sepc){
-      return read_csr_sepc(a, status);
-    }else if(csraddr == scause){
-      return read_csr_scause(a, status);
-    }else if(csraddr == stval){
-      return read_csr_stval(a, status);
-    }else if(csraddr == sip){
-      return read_csr_sip(a, status);
-    }else if(csraddr == satp){
-      return read_csr_satp(a, status);
-    }else if(csraddr == mstatus){
-      return read_csr_mstatus(a, status);
-    }else if(csraddr == misa){
-      return read_csr_misa(a, status);
-    }else if(csraddr == medeleg){
-      return read_csr_medeleg(a, status);
-    }else if(csraddr == mideleg){
-      return read_csr_mideleg(a, status);
-    }else if(csraddr == mie){
-      return read_csr_mie(a, status);
-    }else if(csraddr == mtvec){
-      return read_csr_mtvec(a, status);
-    }else if(csraddr == mcounteren){
-      return read_csr_mcounteren(a, status);
-    }else if(csraddr == mscratch){
-      return read_csr_mscratch(a, status);
-    }else if(csraddr == mepc){
-      return read_csr_mepc(a, status);
-    }else if(csraddr == mcause){
-      return read_csr_mcause(a, status);
-    }else if(csraddr == mtval){
-      return read_csr_mtval(a, status);
-    }else if(csraddr == mip){
-      return read_csr_mip(a, status);
-    }else if(csraddr == mcycle){
-      return read_csr_mcycle(a, status);
-    }else if(csraddr == minstret){
-      return read_csr_minstret(a, status);
-    }else if(csraddr == mvendorid){
-      return read_csr_mvendorid(a, status);
-    }else if(csraddr == marchid){
-      return read_csr_marchid(a, status);
-    }else if(csraddr == mimpid){
-      return read_csr_mimpid(a, status);
+    // TO-DO: Change this to binary search or mapping to increase performance
+    // (in the meantime, pray for solidity devs to add switch statements)
+    if(csr_addr == ucycle) {
+      //return read_csr_cycle();
+    }else if(csr_addr == uinstret){
+      //return read_csr_instret();
+    }else if(csr_addr == utime){
+      //return read_csr_time();
+    }else if(csr_addr == sstatus){
+      //return read_csr_sstatus();
+    }else if(csr_addr == sie){
+      //return read_csr_sie();
+    }else if(csr_addr == stvec){
+      //return read_csr_stvec();
+    }else if(csr_addr == scounteren){
+      //return read_csr_scounteren();
+    }else if(csr_addr == sscratch){
+      //return read_csr_sscratch();
+    }else if(csr_addr == sepc){
+      //return read_csr_sepc();
+    }else if(csr_addr == scause){
+      //return read_csr_scause();
+    }else if(csr_addr == stval){
+      //return read_csr_stval();
+    }else if(csr_addr == sip){
+      //return read_csr_sip();
+    }else if(csr_addr == satp){
+      //return read_csr_satp();
+    }else if(csr_addr == mstatus){
+      //return read_csr_mstatus();
+    }else if(csr_addr == misa){
+      //return read_csr_misa();
+    }else if(csr_addr == medeleg){
+      //return read_csr_medeleg();
+    }else if(csr_addr == mideleg){
+      //return read_csr_mideleg();
+    }else if(csr_addr == mie){
+      //return read_csr_mie();
+    }else if(csr_addr == mtvec){
+      //return read_csr_mtvec();
+    }else if(csr_addr == mcounteren){
+      //return read_csr_mcounteren();
+    }else if(csr_addr == mscratch){
+      //return read_csr_mscratch();
+    }else if(csr_addr == mepc){
+      //return read_csr_mepc();
+    }else if(csr_addr == mcause){
+      //return read_csr_mcause();
+    }else if(csr_addr == mtval){
+      //return read_csr_mtval();
+    }else if(csr_addr == mip){
+      //return read_csr_mip();
+    }else if(csr_addr == mcycle){
+      //return read_csr_mcycle();
+    }else if(csr_addr == minstret){
+      //return read_csr_minstret();
+    }else if(csr_addr == mvendorid){
+      //return read_csr_mvendorid();
+    }else if(csr_addr == marchid){
+      //return read_csr_marchid();
+    }else if(csr_addr == mimpid){
+      //return read_csr_mimpid();
     }
     // All hardwired to zero
-    else if(csraddr == tselect || csraddr == tdata1 || csraddr == tdata2 || csraddr == tdata3 ||  csraddr == mhartid){
-      return (0, true);
+    else if(csr_addr == tselect || csr_addr == tdata1 || csr_addr == tdata2 || csr_addr == tdata3 ||  csr_addr == mhartid){
+      return (true, 0);
     }
   }
 
@@ -147,8 +150,26 @@ library CSR {
   // Bits csr[9:8] encode the CSR's privilege level (i.e lowest privilege level
   // that can access that CSR.
   // Reference: riscv-privileged-v1.10 - section 2.1 - page 7.
-  function csr_priv(uint32 csr_address) internal returns(uint32) {
-    return (csr_address >> 8) & 3;
+  function csr_priv(uint32 csr_addr) internal returns(uint32) {
+    return (csr_addr >> 8) & 3;
+  }
+
+  // Check if counter is enabled. mcounteren control the availability of the 
+  // hardware performance monitoring counter to the next-lowest priv level.
+  // Reference: riscv-privileged-v1.10 - section 3.1.17 - page 32.
+  function rdcounteren(MemoryInteractor mi, uint256 mmIndex, uint32 csr_addr) 
+  internal returns (bool){
+    uint64 counteren = RiscVConstants.MCOUNTEREN_RW_MASK();
+    uint64 priv = mi.read_iflags_PRV(mmIndex);
+
+    if (priv < RiscVConstants.PRV_M()) {
+      counteren &= mi.read_mcounteren(mmIndex);
+      if (priv < RiscVConstants.PRV_S()) {
+        counteren &= mi.read_scounteren(mmIndex);
+      }
+    }
+    return (((counteren >> (csr_addr & 0x1f)) & 1) != 0);
+
   }
 }
 
