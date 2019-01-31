3d344689d1810ff2dd9dbd321dd4a53d45dbc67e
felipeargento <felipeargento@gmail.com>
2019-01-31 19:13:38 +0800

    Added migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,12 +3,14 @@ var RiscVDecoder = artifacts.require("./RiscVDecoder.sol");
 var ShadowAddresses = artifacts.require("./ShadowAddresses.sol");
 var RiscVConstants = artifacts.require("./RiscVConstants.sol");
 var BranchInstructions = artifacts.require("./RiscVInstructions/BranchInstructions.sol");
+var RealTimeClock = artifacts.require("./RealTimeClock.sol");
 var ArithmeticInstructions = artifacts.require("./RiscVInstructions/ArithmeticInstructions.sol");
 var ArithmeticImmediateInstructions = artifacts.require("./RiscVInstructions/ArithmeticImmediateInstructions.sol");
 var BitsManipulationLibrary = artifacts.require("./lib/BitsManipulationLibrary.sol");
 var Execute = artifacts.require("./Execute.sol");
 var Fetch = artifacts.require("./Fetch.sol");
 var PMA = artifacts.require("./PMA.sol");
+var CSR = artifacts.require("./CSR.sol");
 var Interrupts = artifacts.require("./Interrupts.sol");
 
 //Contracts
@@ -24,6 +26,7 @@ module.exports = function(deployer) {
   deployer.deploy(RiscVConstants);
   deployer.deploy(BitsManipulationLibrary);
   deployer.deploy(RiscVDecoder);
+  deployer.deploy(RealTimeClock);
 
   deployer.link(RiscVDecoder, BranchInstructions);
   deployer.link(RiscVDecoder, ArithmeticInstructions);
@@ -34,6 +37,11 @@ module.exports = function(deployer) {
   deployer.deploy(BranchInstructions);
   deployer.deploy(PMA);
 
+  //Link all libraries to CSR
+  deployer.link(RealTimeClock, CSR);
+  deployer.link(RiscVDecoder, CSR);
+  deployer.link(RiscVConstants, CSR);
+  deployer.deploy(CSR);
 
   //Link all libraries to Step
   deployer.link(RiscVDecoder, Step);
@@ -55,6 +63,7 @@ module.exports = function(deployer) {
   deployer.link(BranchInstructions, Execute);
   deployer.link(ArithmeticInstructions, Execute);
   deployer.link(ArithmeticImmediateInstructions, Execute);
+  deployer.link(CSR, Execute);
   deployer.deploy(Execute);
   deployer.link(Execute, Step);
   
@@ -66,6 +75,7 @@ module.exports = function(deployer) {
   
 
   deployer.link(BitsManipulationLibrary, MemoryInteractor);
+  deployer.link(ShadowAddresses, MemoryInteractor);
   deployer.deploy(AddressTracker);
   deployer.deploy(MMInstantiator).then(function(){
     return deployer.deploy(MemoryInteractor, AddressTracker.address);
