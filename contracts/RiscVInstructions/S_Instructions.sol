ae79697305b69633c409093a006359a4bc622c4d
felipeargento <felipeargento@gmail.com>
2019-02-11 19:17:16 +0800

    wordSize is now number of bits not of bytes

--- contracts/RiscVInstructions/S_Instructions.sol
+++ contracts/RiscVInstructions/S_Instructions.sol
@@ -17,28 +17,28 @@ library S_Instructions {
   function SB(MemoryInteractor mi, uint256 mmIndex, uint64 pc, uint32 insn)
   public returns(bool){
     (uint64 vaddr, int32 imm, uint64 val) = get_rs1_imm_rs2(mi, mmIndex, insn);
-    // 1 == sizeof(uint8)
-    return VirtualMemory.write_virtual_memory(mi, mmIndex, 1, vaddr + uint64(imm), val);
+    // 8 == uint8
+    return VirtualMemory.write_virtual_memory(mi, mmIndex, 8, vaddr + uint64(imm), val);
   }
 
   function SH(MemoryInteractor mi, uint256 mmIndex, uint64 pc, uint32 insn)
   public returns(bool){
     (uint64 vaddr, int32 imm, uint64 val) = get_rs1_imm_rs2(mi, mmIndex, insn);
-    // 2 == sizeof(uint16)
-    return VirtualMemory.write_virtual_memory(mi, mmIndex, 2, vaddr + uint64(imm), val);
+    // 16 == uint16
+    return VirtualMemory.write_virtual_memory(mi, mmIndex, 16, vaddr + uint64(imm), val);
   }
 
   function SW(MemoryInteractor mi, uint256 mmIndex, uint64 pc, uint32 insn)
   public returns(bool){
     (uint64 vaddr, int32 imm, uint64 val) = get_rs1_imm_rs2(mi, mmIndex, insn);
-    // 4 == sizeof(uint32)
-    return VirtualMemory.write_virtual_memory(mi, mmIndex, 4, vaddr + uint64(imm), val);
+    // 32 == uint32
+    return VirtualMemory.write_virtual_memory(mi, mmIndex, 32, vaddr + uint64(imm), val);
   }
-  
+
   function SD(MemoryInteractor mi, uint256 mmIndex, uint64 pc, uint32 insn)
   public returns(bool){
     (uint64 vaddr, int32 imm, uint64 val) = get_rs1_imm_rs2(mi, mmIndex, insn);
-    // 8 == sizeof(uint64)
-    return VirtualMemory.write_virtual_memory(mi, mmIndex, 8, vaddr + uint64(imm), val);
+    // 64 == uint64
+    return VirtualMemory.write_virtual_memory(mi, mmIndex, 64, vaddr + uint64(imm), val);
   }
 }
