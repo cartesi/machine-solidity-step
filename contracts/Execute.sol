a30dc8292adbb6a7aeb14ca4fcb0476fdbde1d36
Felipe Argento <felipe@cartesi.io>
2019-03-25 18:53:39 +0800

    Added JALR instruction

--- contracts/Execute.sol
+++ contracts/Execute.sol
@@ -222,6 +222,26 @@ library Execute {
     return execute_jump(mi, mmIndex, new_pc);
   }
 
+  // JALR (i.e Jump and Link Register). uses the I-type encoding. The target
+  // address is obtained by adding the 12-bit signed I-immediate to the register 
+  // rs1, then setting the least-significant bit of the result to zero. 
+  // The address of the instruction following the jump (pc+4) is written to register rd
+  // Reference: riscv-spec-v2.2.pdf -  Section 2.5 - page 16
+  function execute_jalr(MemoryInteractor mi, uint256 mmIndex, uint32 insn, uint64 pc)
+  public returns (execute_status){
+    uint64 new_pc = uint64(int64(mi.read_x(mmIndex, RiscVDecoder.insn_rs1(insn)))) & ~uint64(1);
+
+    if((new_pc & 3) != 0){
+      return raise_misaligned_fetch_exception(mi, mmIndex, new_pc);
+    }
+    uint32 rd = RiscVDecoder.insn_rd(insn);
+
+    if(rd != 0){
+      mi.write_x(mmIndex, rd, pc + 4);
+    }
+    return execute_jump(mi, mmIndex, new_pc);
+  }
+
   //AUIPC forms a 32-bit offset from the 20-bit U-immediate, filling in the 
   // lowest 12 bits with zeros, adds this offset to pc and store the result on rd.
   // Reference: riscv-spec-v2.2.pdf -  Page 14
@@ -244,7 +264,7 @@ library Execute {
     uint32 rd = RiscVDecoder.insn_rd(insn);
 
     if(rd != 0){
-      mi.write_x(mmIndex, rd, RiscVDecoder.insn_U_imm(insn));
+      mi.write_x(mmIndex, rd, uint64(RiscVDecoder.insn_U_imm(insn)));
     }
     return advance_to_next_insn(mi, mmIndex, pc);
   }
