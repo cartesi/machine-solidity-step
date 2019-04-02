681f8805adb03035503db1fe23aa3aaa598c9060
felipeargento <felipeargento@gmail.com>
2019-04-02 15:15:51 +0800

    Added AMO_D instructions

--- contracts/RiscVInstructions/AtomicInstructions.sol
+++ contracts/RiscVInstructions/AtomicInstructions.sol
@@ -10,15 +10,9 @@ library AtomicInstructions {
   function execute_AMO_part1(MemoryInteractor mi, uint256 mmIndex, uint64 pc, uint32 insn, uint256 wordSize)
   internal returns (uint64, uint64, uint64, bool){
     uint64 vaddr = mi.read_x(mmIndex, RiscVDecoder.insn_rs1(insn));
-    bool succ;
-    uint64 tmp_valm;
 
-    if(wordSize == 32){
-      (succ, tmp_valm) = VirtualMemory.read_virtual_memory(mi, mmIndex, 32, vaddr);
-    } else {
-      // wordSize == 64
-      (succ, tmp_valm)  = VirtualMemory.read_virtual_memory(mi, mmIndex, 64, vaddr);
-    }
+    (bool succ, uint64 tmp_valm) = VirtualMemory.read_virtual_memory(mi, mmIndex, wordSize, vaddr);
+
     if(!succ){
       return (0, 0, 0, false);
     }
@@ -27,9 +21,9 @@ library AtomicInstructions {
     return (tmp_valm, tmp_valr, vaddr, true);
   }
 
-  function execute_AMO_part2(MemoryInteractor mi, uint256 mmIndex, uint64 pc, uint32 insn, uint64 vaddr, int32 valr, int32 valm)
+  function execute_AMO_part2(MemoryInteractor mi, uint256 mmIndex, uint64 pc, uint32 insn, uint64 vaddr, int32 valr, int32 valm, uint64 wordSize)
   internal returns (bool) {
-    if (!VirtualMemory.write_virtual_memory(mi, mmIndex, 32, vaddr, uint64(valr))) {
+    if (!VirtualMemory.write_virtual_memory(mi, mmIndex, wordSize, vaddr, uint64(valr))) {
       return false;
     }
     uint32 rd = RiscVDecoder.insn_rd(insn);
@@ -43,56 +37,113 @@ library AtomicInstructions {
   function execute_AMOSWAP_W(MemoryInteractor mi, uint256 mmIndex, uint64 pc, uint32 insn)
   public returns(bool) {
     (uint64 valm, uint64 valr, uint64 vaddr, bool succ) = execute_AMO_part1(mi, mmIndex, pc, insn, 32);
-    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32(valr), int32(0));
+    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32(valr), int32(0), 32);
   }
 
   function execute_AMOADD_W(MemoryInteractor mi, uint256 mmIndex, uint64 pc, uint32 insn)
   public returns(bool) {
     (uint64 valm, uint64 valr, uint64 vaddr, bool succ) = execute_AMO_part1(mi, mmIndex, pc, insn, 32);
-    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32(valm + valr), int32(valm));
+    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32(valm + valr), int32(valm), 32);
   }
 
   function execute_AMOXOR_W(MemoryInteractor mi, uint256 mmIndex, uint64 pc, uint32 insn)
   public returns(bool) {
     (uint64 valm, uint64 valr, uint64 vaddr, bool succ) = execute_AMO_part1(mi, mmIndex, pc, insn, 32);
-    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32(valm ^ valr), int32(valm));
+    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32(valm ^ valr), int32(valm), 32);
   }
 
   function execute_AMOAND_W(MemoryInteractor mi, uint256 mmIndex, uint64 pc, uint32 insn)
   public returns(bool) {
     (uint64 valm, uint64 valr, uint64 vaddr, bool succ) = execute_AMO_part1(mi, mmIndex, pc, insn, 32);
-    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32(valm & valr), int32(valm));
+    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32(valm & valr), int32(valm), 32);
   }
 
   function execute_AMOOR_W(MemoryInteractor mi, uint256 mmIndex, uint64 pc, uint32 insn)
   public returns(bool) {
     (uint64 valm, uint64 valr, uint64 vaddr, bool succ) = execute_AMO_part1(mi, mmIndex, pc, insn, 32);
-    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32(valm | valr), int32(valm));
+    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32(valm | valr), int32(valm), 32);
 
   }
 
   function execute_AMOMIN_W(MemoryInteractor mi, uint256 mmIndex, uint64 pc, uint32 insn)
   public returns(bool) {
     (uint64 valm, uint64 valr, uint64 vaddr, bool succ) = execute_AMO_part1(mi, mmIndex, pc, insn, 32);
-    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32( valm < valr? valm : valr), int32(valm));
+    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32( valm < valr? valm : valr), int32(valm), 32);
   }
 
   function execute_AMOMAX_W(MemoryInteractor mi, uint256 mmIndex, uint64 pc, uint32 insn)
   public returns(bool) {
     (uint64 valm, uint64 valr, uint64 vaddr, bool succ) = execute_AMO_part1(mi, mmIndex, pc, insn, 32);
-    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32(valm > valr? valm : valr), int32(valm));
+    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32(valm > valr? valm : valr), int32(valm), 32);
   }
 
   function execute_AMOMINU_W(MemoryInteractor mi, uint256 mmIndex, uint64 pc, uint32 insn)
   public returns(bool) {
     (uint64 valm, uint64 valr, uint64 vaddr, bool succ) = execute_AMO_part1(mi, mmIndex, pc, insn, 32);
-    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32(uint32(valm) < uint32(valr)? valm : valr), int32(valm));
+    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32(uint32(valm) < uint32(valr)? valm : valr), int32(valm), 32);
   }
 
   function execute_AMOMAXU_W(MemoryInteractor mi, uint256 mmIndex, uint64 pc, uint32 insn)
   public returns(bool) {
     (uint64 valm, uint64 valr, uint64 vaddr, bool succ) = execute_AMO_part1(mi, mmIndex, pc, insn, 32);
-    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32(uint32(valm) > uint32(valr)? valm : valr), int32(valm));
+    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32(uint32(valm) > uint32(valr)? valm : valr), int32(valm), 32);
   }
+
+  // TO-DO: valm should actually be zero?
+  function execute_AMOSWAP_D(MemoryInteractor mi, uint256 mmIndex, uint64 pc, uint32 insn)
+  public returns(bool) {
+    (uint64 valm, uint64 valr, uint64 vaddr, bool succ) = execute_AMO_part1(mi, mmIndex, pc, insn, 64);
+    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32(valr), int32(0), 64);
+  }
+
+  function execute_AMOADD_D(MemoryInteractor mi, uint256 mmIndex, uint64 pc, uint32 insn)
+  public returns(bool) {
+    (uint64 valm, uint64 valr, uint64 vaddr, bool succ) = execute_AMO_part1(mi, mmIndex, pc, insn, 64);
+    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32(valm + valr), int32(valm), 64);
+  }
+
+  function execute_AMOXOR_D(MemoryInteractor mi, uint256 mmIndex, uint64 pc, uint32 insn)
+  public returns(bool) {
+    (uint64 valm, uint64 valr, uint64 vaddr, bool succ) = execute_AMO_part1(mi, mmIndex, pc, insn, 64);
+    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32(valm ^ valr), int32(valm), 64);
+  }
+
+  function execute_AMOAND_D(MemoryInteractor mi, uint256 mmIndex, uint64 pc, uint32 insn)
+  public returns(bool) {
+    (uint64 valm, uint64 valr, uint64 vaddr, bool succ) = execute_AMO_part1(mi, mmIndex, pc, insn, 64);
+    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32(valm & valr), int32(valm), 64);
+  }
+
+  function execute_AMOOR_D(MemoryInteractor mi, uint256 mmIndex, uint64 pc, uint32 insn)
+  public returns(bool) {
+    (uint64 valm, uint64 valr, uint64 vaddr, bool succ) = execute_AMO_part1(mi, mmIndex, pc, insn, 64);
+    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32(valm | valr), int32(valm), 64);
+
+  }
+
+  function execute_AMOMIN_D(MemoryInteractor mi, uint256 mmIndex, uint64 pc, uint32 insn)
+  public returns(bool) {
+    (uint64 valm, uint64 valr, uint64 vaddr, bool succ) = execute_AMO_part1(mi, mmIndex, pc, insn, 64);
+    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32( valm < valr? valm : valr), int32(valm), 64);
+  }
+
+  function execute_AMOMAX_D(MemoryInteractor mi, uint256 mmIndex, uint64 pc, uint32 insn)
+  public returns(bool) {
+    (uint64 valm, uint64 valr, uint64 vaddr, bool succ) = execute_AMO_part1(mi, mmIndex, pc, insn, 64);
+    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32(valm > valr? valm : valr), int32(valm), 64);
+  }
+
+  function execute_AMOMINU_D(MemoryInteractor mi, uint256 mmIndex, uint64 pc, uint32 insn)
+  public returns(bool) {
+    (uint64 valm, uint64 valr, uint64 vaddr, bool succ) = execute_AMO_part1(mi, mmIndex, pc, insn, 64);
+    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32(uint32(valm) < uint32(valr)? valm : valr), int32(valm), 64);
+  }
+
+  function execute_AMOMAXU_D(MemoryInteractor mi, uint256 mmIndex, uint64 pc, uint32 insn)
+  public returns(bool) {
+    (uint64 valm, uint64 valr, uint64 vaddr, bool succ) = execute_AMO_part1(mi, mmIndex, pc, insn, 64);
+    return execute_AMO_part2(mi, mmIndex, pc, insn, vaddr, int32(uint32(valm) > uint32(valr)? valm : valr), int32(valm), 64);
+  }
+
 }
 
