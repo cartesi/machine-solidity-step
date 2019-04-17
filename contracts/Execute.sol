b4265b15c014f5e78181e4f7a38052fc55624927
felipeargento <felipeargento@gmail.com>
2019-04-17 21:37:49 +0800

    Removing JAL and JALR from execute

--- contracts/Execute.sol
+++ contracts/Execute.sol
@@ -11,6 +11,7 @@ import "./RiscVInstructions/BranchInstructions.sol";
 import "./RiscVInstructions/ArithmeticInstructions.sol";
 import "./RiscVInstructions/ArithmeticImmediateInstructions.sol";
 import "./RiscVInstructions/S_Instructions.sol";
+import "./RiscVInstructions/StandAloneInstructions.sol";
 import "./RiscVInstructions/AtomicInstructions.sol";
 import "./RiscVInstructions/EnvTrapIntInstructions.sol";
 import {Exceptions} from "../contracts/Exceptions.sol";
@@ -121,73 +122,6 @@ library Execute {
       //return advance_to_raised_exception()
       return execute_status.retired;
     }
-  }
-
-
-  // JAL (i.e Jump and Link). J_immediate encondes a signed offset in multiples
-  // of 2 bytes. The offset is added to pc and JAL stores the address of the jump
-  // (pc + 4) to the rd register.
-  // Reference: riscv-spec-v2.2.pdf -  Section 2.5 - page 16
-  function execute_jal(MemoryInteractor mi, uint256 mmIndex, uint32 insn, uint64 pc)
-  public returns (execute_status){
-    uint64 new_pc = pc + uint64(RiscVDecoder.insn_J_imm(insn));
-
-    if((new_pc & 3) != 0){
-      return raise_misaligned_fetch_exception(mi, mmIndex, new_pc);
-    }
-    uint32 rd = RiscVDecoder.insn_rd(insn);
-
-    if(rd != 0){
-      mi.write_x(mmIndex, rd, pc + 4);
-    }
-    return execute_jump(mi, mmIndex, new_pc);
-  }
-
-  // JALR (i.e Jump and Link Register). uses the I-type encoding. The target
-  // address is obtained by adding the 12-bit signed I-immediate to the register 
-  // rs1, then setting the least-significant bit of the result to zero. 
-  // The address of the instruction following the jump (pc+4) is written to register rd
-  // Reference: riscv-spec-v2.2.pdf -  Section 2.5 - page 16
-  function execute_jalr(MemoryInteractor mi, uint256 mmIndex, uint32 insn, uint64 pc)
-  public returns (execute_status){
-    uint64 new_pc = uint64(int64(mi.read_x(mmIndex, RiscVDecoder.insn_rs1(insn)))) & ~uint64(1);
-
-    if((new_pc & 3) != 0){
-      return raise_misaligned_fetch_exception(mi, mmIndex, new_pc);
-    }
-    uint32 rd = RiscVDecoder.insn_rd(insn);
-
-    if(rd != 0){
-      mi.write_x(mmIndex, rd, pc + 4);
-    }
-    return execute_jump(mi, mmIndex, new_pc);
-  }
-
-  //AUIPC forms a 32-bit offset from the 20-bit U-immediate, filling in the 
-  // lowest 12 bits with zeros, adds this offset to pc and store the result on rd.
-  // Reference: riscv-spec-v2.2.pdf -  Page 14
-  function execute_auipc(MemoryInteractor mi, uint256 mmIndex, uint32 insn, uint64 pc)
-  public returns (execute_status){
-    uint32 rd = RiscVDecoder.insn_rd(insn);
-
-    if(rd != 0){
-      mi.write_x(mmIndex, rd, pc + uint64(RiscVDecoder.insn_U_imm(insn)));
-    }
-    return advance_to_next_insn(mi, mmIndex, pc);
-  }
-
-  // LUI (i.e load upper immediate). Is used to build 32-bit constants and uses 
-  // the U-type format. LUI places the U-immediate value in the top 20 bits of
-  // the destination register rd, filling in the lowest 12 bits with zeros
-  // Reference: riscv-spec-v2.2.pdf -  Section 2.5 - page 13
-  function execute_lui(MemoryInteractor mi, uint256 mmIndex, uint32 insn, uint64 pc)
-  public returns (execute_status){
-    uint32 rd = RiscVDecoder.insn_rd(insn);
-
-    if(rd != 0){
-      mi.write_x(mmIndex, rd, uint64(RiscVDecoder.insn_U_imm(insn)));
-    }
-    return advance_to_next_insn(mi, mmIndex, pc);
   }
 
   function execute_jump(MemoryInteractor mi, uint256 mmIndex, uint64 new_pc) public returns (execute_status){
@@ -956,7 +890,8 @@ library Execute {
         }
       }else if(opcode == 0x0017){
         /*opcode == 0x0017*/
-        return execute_auipc(mi, mmIndex, insn, pc);
+        StandAloneInstructions.execute_auipc(mi, mmIndex, insn, pc);
+        return advance_to_next_insn(mi, mmIndex, pc);
       }
     }else if (opcode > 0x002f){
       if (opcode < 0x0063){
@@ -971,20 +906,31 @@ library Execute {
         }else if(opcode == 0x0037){
           /*opcode == 0x0037*/
           //return "LUI";
-          return execute_lui(mi, mmIndex, insn, pc);
+          StandAloneInstructions.execute_lui(mi, mmIndex, insn, pc);
+          return advance_to_next_insn(mi, mmIndex, pc);
         }
       }else if (opcode > 0x0063){
         if(opcode == 0x0067){
           /*opcode == 0x0067*/
           //return "JALR";
-          return execute_jalr(mi, mmIndex, insn, pc);
+          (bool succ, uint64 new_pc) = StandAloneInstructions.execute_jalr(mi, mmIndex, insn, pc);
+          if (succ) {
+            return execute_jump(mi, mmIndex, new_pc);
+          } else {
+            return raise_misaligned_fetch_exception(mi, mmIndex, new_pc);
+          }
         }else if(opcode == 0x0073){
           /*opcode == 0x0073*/
           return csr_env_trap_int_mm_funct3(mi, mmIndex, insn, pc);
         }else if(opcode == 0x006f){
           /*opcode == 0x006f*/
           //return "JAL";
-          return execute_jal(mi, mmIndex, insn, pc);
+          (bool succ, uint64 new_pc) = StandAloneInstructions.execute_jal(mi, mmIndex, insn, pc);
+          if (succ) {
+            return execute_jump(mi, mmIndex, new_pc);
+          } else {
+            return raise_misaligned_fetch_exception(mi, mmIndex, new_pc);
+          }
         }
       }else if (opcode == 0x0063){
         /*opcode == 0x0063*/
