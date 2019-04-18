34850e576e06b15e4e23a56f4e7bac30fc93a12e
Augusto Teixeira <augusto@impa.br>
2019-04-18 11:59:50 -0300

    Add address to deploy in demo

--- truffle.js
+++ truffle.js
@@ -4,7 +4,8 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*", // Match any network id
-      gas: 9007199254740991
+      gas: 9007199254740991,
+      from: "0x8b5432ca3423f3c310eba126c1d15809c61aa0a9"
     },
     solc: {
      optimizer: { // Turning on compiler optimization that removes some local variables during compilation
