81013eeb067339cc6ce7eb137060c238a5b23430
felipeargento <felipeargento@gmail.com>
2019-06-12 16:49:55 +0800

    Removing execute functions from CSR

--- contracts/CSR.sol
+++ contracts/CSR.sol
@@ -3,9 +3,7 @@ pragma solidity ^0.5.0;
 
 import "../contracts/MemoryInteractor.sol";
 import "../contracts/RiscVConstants.sol";
-import "../contracts/RiscVDecoder.sol";
 import "../contracts/CSR_reads.sol";
-//import "../contracts/CSR_writes.sol";
 
 library CSR {
 
@@ -53,46 +51,6 @@ library CSR {
   uint32 constant tdata1 = 0x7a1;
   uint32 constant tdata2 = 0x7a2;
   uint32 constant tdata3 = 0x7a3;
-
-//  uint256 constant 0 = 0;
-//  uint256 constant 1 = 1;
-
-  uint256 constant CSRRS_code = 0;
-  uint256 constant CSRRC_code = 1;
-
-  uint256 constant CSRRSI_code = 0;
-  uint256 constant CSRRCI_code = 1;
-
-
-  function execute_CSRRW(MemoryInteractor mi, uint256 mmIndex, uint32 insn)
-  public returns(uint64) {
-    return mi.read_x(mmIndex, RiscVDecoder.insn_rs1(insn)); 
-  }
-
-  function execute_CSRRWI(uint32 insn)
-  public returns(uint64) {
-    return uint64(RiscVDecoder.insn_rs1(insn));
-  }
-
-  function execute_CSRRS(uint64 csr, uint64 rs1)
-  public returns(uint64) {
-    return csr | rs1;
-  }
-
-  function execute_CSRRC(uint64 csr, uint64 rs1)
-  public returns(uint64) {
-    return csr & ~rs1;
-  }
-
-  function execute_CSRRSI(uint64 csr, uint32 rs1)
-  public returns(uint64) {
-    return csr | rs1;
-  }
-
-  function execute_CSRRCI(uint64 csr, uint32 rs1)
-  public returns(uint64) {
-    return csr & ~rs1;
-  }
 
   function read_csr(MemoryInteractor mi, uint256 mmIndex, uint32 csr_addr)
   public returns (bool, uint64) {
@@ -328,118 +286,6 @@ library CSR {
     return ((csr_addr & 0xc00) == 0xc00);
   }
 
-  function execute_csr_SC(MemoryInteractor mi, uint256 mmIndex, uint32 insn, uint256 insncode)
-  public returns (bool) {
-    uint32 csr_address = RiscVDecoder.insn_I_uimm(insn);
-
-    bool status = false;
-    uint64 csrval = 0;
-
-    (status, csrval) = read_csr(mi, mmIndex, csr_address);
-
-    if (!status) {
-      //return raise_illegal_insn_exception(mi, mmIndex, insn);
-      return false;
-    }
-    uint32 rs1 = RiscVDecoder.insn_rs1(insn);
-    uint64 rs1val = mi.read_x(mmIndex, rs1);
-    uint32 rd = RiscVDecoder.insn_rd(insn);
-
-    if (rd != 0) {
-      mi.write_x(mmIndex, rd, csrval);
-    }
-
-    uint64 exec_value = 0;
-    if (insncode == CSRRS_code) {
-      exec_value = execute_CSRRS(csrval, rs1val);
-    } else {
-      // insncode == CSRRC_code
-      exec_value = execute_CSRRC(csrval, rs1val);
-    }
-    if (rs1 != 0) {
-      if (!write_csr(mi, mmIndex, csr_address, exec_value)){
-        //return raise_illegal_insn_exception(mi, mmIndex, insn);
-        return false;
-      }
-    }
-    //return advance_to_next_insn(mi, mmIndex, pc);
-    return true;
-  }
-
-   function execute_csr_SCI(MemoryInteractor mi, uint256 mmIndex, uint32 insn, uint256 insncode)
-  public returns (bool){
-    uint32 csr_address = RiscVDecoder.insn_I_uimm(insn);
-
-    bool status = false;
-    uint64 csrval = 0;
-
-    (status, csrval) = read_csr(mi, mmIndex, csr_address);
-
-    if (!status) {
-      //return raise_illegal_insn_exception(mi, mmIndex, insn);
-      return false;
-    }
-    uint32 rs1 = RiscVDecoder.insn_rs1(insn);
-    uint32 rd = RiscVDecoder.insn_rd(insn);
-
-    if (rd != 0) {
-      mi.write_x(mmIndex, rd, csrval);
-    }
-
-    uint64 exec_value = 0;
-    if (insncode == CSRRSI_code) {
-      exec_value = execute_CSRRSI(csrval, rs1);
-    } else {
-      // insncode == CSRRCI_code
-      exec_value = execute_CSRRCI(csrval, rs1);
-    }
-
-    if (rs1 != 0) {
-      if (!write_csr(mi, mmIndex, csr_address, exec_value)){
-        //return raise_illegal_insn_exception(mi, mmIndex, insn);
-        return false;
-      }
-    }
-    //return advance_to_next_insn(mi, mmIndex, pc);
-    return true;
-  }
-
-  function execute_csr_RW(MemoryInteractor mi, uint256 mmIndex, uint32 insn, uint256 insncode)
-  public returns (bool) {
-    uint32 csr_address = RiscVDecoder.insn_I_uimm(insn);
-
-    bool status = true;
-    uint64 csrval = 0;
-    uint64 rs1val = 0;
-
-    if (insncode == 0) {
-      rs1val = execute_CSRRW(mi, mmIndex, insn);
-    } else {
-      // insncode == 1
-      rs1val = execute_CSRRWI(insn);
-    }
-
-    uint32 rd = RiscVDecoder.insn_rd(insn);
-
-    if (rd != 0){
-      (status, csrval) = read_csr(mi, mmIndex, csr_address);
-    }
-    if (!status) {
-      //return raise_illegal_insn_exception(mi, mmIndex, insn);
-      return false;
-    }
-
-    if (!write_csr(mi, mmIndex, csr_address, rs1val)){
-      //return raise_illegal_insn_exception(mi, mmIndex, insn);
-      return false;
-    }
-    if (rd != 0){
-      mi.write_x(mmIndex, rd, csrval);
-    }
-    //return advance_to_next_insn(mi, mmIndex, pc);
-    return true;
-  }
-
   function write_csr_mstatus(MemoryInteractor mi, uint256 mmIndex, uint64 val) 
   internal returns(bool){
     uint64 c_mstatus = mi.read_mstatus(mmIndex) & RiscVConstants.MSTATUS_R_MASK();
@@ -452,16 +298,5 @@ library CSR {
     mi.write_mstatus(mmIndex, c_mstatus);
     return true;
   }
-
- // getters
-//  function get_0() public returns (uint256) {return 0;}
-//  function get_1() public returns (uint256) {return 1;}
-//
-//  function get_CSRRS_code() public returns (uint256) {return CSRRS_code; }
-//  function get_CSRRC_code() public returns (uint256) {return CSRRC_code; }
-//
-//  function get_CSRRSI_code() public returns (uint256) {return CSRRSI_code;}
-//  function get_CSRRCI_code() public returns (uint256) {return CSRRCI_code;}
-
 }
 
