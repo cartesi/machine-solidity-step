2e35a3f4bece3fcd3c4b717374fa3fcec74277d8
felipeargento <felipeargento@gmail.com>
2019-06-23 22:34:02 +0800

    Applying linter to instructions libraries

--- contracts/RiscVInstructions/BranchInstructions.sol
+++ contracts/RiscVInstructions/BranchInstructions.sol
@@ -4,80 +4,77 @@ pragma solidity ^0.5.0;
 import "../../contracts/MemoryInteractor.sol";
 import "../../contracts/RiscVDecoder.sol";
 
+
 library BranchInstructions {
 
-  function get_rs1_rs2(MemoryInteractor mi, uint256 mmIndex, uint32 insn) internal 
-  returns(uint64 rs1, uint64 rs2) {
-    rs1 = mi.read_x(mmIndex, RiscVDecoder.insn_rs1(insn));
-    rs2 = mi.read_x(mmIndex, RiscVDecoder.insn_rs2(insn));
-  }
-
-  function execute_BEQ(MemoryInteractor mi, uint256 mmIndex, uint32 insn) public returns (bool){
-    (uint64 rs1, uint64 rs2) = get_rs1_rs2(mi, mmIndex, insn);
-    return rs1 == rs2;
-  }
-
-  function execute_BNE(MemoryInteractor mi, uint256 mmIndex, uint32 insn) public returns (bool){
-    (uint64 rs1, uint64 rs2) = get_rs1_rs2(mi, mmIndex, insn);
-    return rs1 != rs2;
-  }
-
-  function execute_BLT(MemoryInteractor mi, uint256 mmIndex, uint32 insn) public returns (bool){
-    (uint64 rs1, uint64 rs2) = get_rs1_rs2(mi, mmIndex, insn);
-    return int64(rs1) < int64(rs2);
-  }
-
-  function execute_BGE(MemoryInteractor mi, uint256 mmIndex, uint32 insn) public returns (bool){
-    (uint64 rs1, uint64 rs2) = get_rs1_rs2(mi, mmIndex, insn);
-    return int64(rs1) >= int64(rs2);
-  }
-
-  function execute_BLTU(MemoryInteractor mi, uint256 mmIndex, uint32 insn) public returns (bool){
-    (uint64 rs1, uint64 rs2) = get_rs1_rs2(mi, mmIndex, insn);
-    return rs1 < rs2;
-  }
-
-  function execute_BGEU(MemoryInteractor mi, uint256 mmIndex, uint32 insn) public returns (bool){
-    (uint64 rs1, uint64 rs2) = get_rs1_rs2(mi, mmIndex, insn);
-    return rs1 >= rs2;
-  }
-
-  /// @notice Given a branch funct3 group instruction, finds the function
-  //  associated with it. Uses binary search for performance.
-  //  @param insn for branch funct3 field.
-  function branch_funct3(MemoryInteractor mi, uint256 mmIndex, uint32 insn)
-  public returns (bool, bool){
-    uint32 funct3 = RiscVDecoder.insn_funct3(insn);
-
-    if(funct3 < 0x0005){
-      if(funct3 == 0x0000){
-        /*funct3 == 0x0000*/
-        //return "BEQ";
-        return (execute_BEQ(mi, mmIndex, insn), true);
-      }else if(funct3 == 0x0004){
-        /*funct3 == 0x0004*/
-        //return "BLT";
-        return (execute_BLT(mi, mmIndex, insn), true);
-      }else if(funct3 == 0x0001){
-        /*funct3 == 0x0001*/
-        //return "BNE";
-        return (execute_BNE(mi, mmIndex, insn), true);
-      }
-    }else if(funct3 > 0x0005){
-      if(funct3 == 0x0007){
-        /*funct3 == 0x0007*/
-        //return "BGEU";
-        return (execute_BGEU(mi, mmIndex, insn), true);
-      }else if(funct3 == 0x0006){
-        /*funct3 == 0x0006*/
-        //return "BLTU";
-        return (execute_BLTU(mi, mmIndex, insn), true);
-      }
-    }else if(funct3 == 0x0005){
-      /*funct3==0x0005*/
-      //return "BGE";
-      return (execute_BGE(mi, mmIndex, insn), true);
+    function getRs1Rs2(MemoryInteractor mi, uint256 mmIndex, uint32 insn) internal
+    returns(uint64 rs1, uint64 rs2)
+    {
+        rs1 = mi.readX(mmIndex, RiscVDecoder.insnRs1(insn));
+        rs2 = mi.readX(mmIndex, RiscVDecoder.insnRs2(insn));
     }
-    return (false, false);
-  }
+
+    function executeBEQ(MemoryInteractor mi, uint256 mmIndex, uint32 insn) public returns (bool) {
+        (uint64 rs1, uint64 rs2) = getRs1Rs2(mi, mmIndex, insn);
+        return rs1 == rs2;
+    }
+
+    function executeBNE(MemoryInteractor mi, uint256 mmIndex, uint32 insn) public returns (bool) {
+        (uint64 rs1, uint64 rs2) = getRs1Rs2(mi, mmIndex, insn);
+        return rs1 != rs2;
+    }
+
+    function executeBLT(MemoryInteractor mi, uint256 mmIndex, uint32 insn) public returns (bool) {
+        (uint64 rs1, uint64 rs2) = getRs1Rs2(mi, mmIndex, insn);
+        return int64(rs1) < int64(rs2);
+    }
+
+    function executeBGE(MemoryInteractor mi, uint256 mmIndex, uint32 insn) public returns (bool) {
+        (uint64 rs1, uint64 rs2) = getRs1Rs2(mi, mmIndex, insn);
+        return int64(rs1) >= int64(rs2);
+    }
+
+    function executeBLTU(MemoryInteractor mi, uint256 mmIndex, uint32 insn) public returns (bool) {
+        (uint64 rs1, uint64 rs2) = getRs1Rs2(mi, mmIndex, insn);
+        return rs1 < rs2;
+    }
+
+    function executeBGEU(MemoryInteractor mi, uint256 mmIndex, uint32 insn) public returns (bool) {
+        (uint64 rs1, uint64 rs2) = getRs1Rs2(mi, mmIndex, insn);
+        return rs1 >= rs2;
+    }
+
+    /// @notice Given a branch funct3 group instruction, finds the function
+    //  associated with it. Uses binary search for performance.
+    //  @param insn for branch funct3 field.
+    function branchFunct3(MemoryInteractor mi, uint256 mmIndex, uint32 insn)
+    public returns (bool, bool)
+    {
+        uint32 funct3 = RiscVDecoder.insnFunct3(insn);
+
+        if (funct3 < 0x0005) {
+            if (funct3 == 0x0000) {
+                /*funct3 == 0x0000*/
+                return (executeBEQ(mi, mmIndex, insn), true);
+            } else if (funct3 == 0x0004) {
+                /*funct3 == 0x0004*/
+                return (executeBLT(mi, mmIndex, insn), true);
+            } else if (funct3 == 0x0001) {
+                /*funct3 == 0x0001*/
+                return (executeBNE(mi, mmIndex, insn), true);
+            }
+        } else if (funct3 > 0x0005) {
+            if (funct3 == 0x0007) {
+                /*funct3 == 0x0007*/
+                return (executeBGEU(mi, mmIndex, insn), true);
+            } else if (funct3 == 0x0006) {
+                /*funct3 == 0x0006*/
+                return (executeBLTU(mi, mmIndex, insn), true);
+            }
+        } else if (funct3 == 0x0005) {
+            /*funct3==0x0005*/
+            return (executeBGE(mi, mmIndex, insn), true);
+        }
+        return (false, false);
+    }
 }
--- contracts/RiscVInstructions/EnvTrapIntInstructions.sol
+++ contracts/RiscVInstructions/EnvTrapIntInstructions.sol
@@ -8,87 +8,127 @@ import "../../contracts/RiscVDecoder.sol";
 import "../../contracts/RiscVConstants.sol";
 import "../../contracts/Exceptions.sol";
 
+
 library EnvTrapIntInstructions {
-  function execute_ECALL(MemoryInteractor mi, uint256 mmIndex, uint32 insn, uint64 pc) public {
-    uint64 priv = mi.read_iflags_PRV(mmIndex);
-    uint64 mtval = mi.read_mtval(mmIndex);
-    // TO-DO: Are parameter valuation order deterministic? If so, we dont need to allocate memory
-    Exceptions.raise_exception(mi, mmIndex, Exceptions.MCAUSE_ECALL_BASE() + priv, mtval);
-  }
-
-  function execute_EBREAK(MemoryInteractor mi, uint256 mmIndex, uint32 insn, uint64 pc) public {
-    Exceptions.raise_exception(mi, mmIndex, Exceptions.MCAUSE_BREAKPOINT(), mi.read_mtval(mmIndex));
-  }
-
-  function execute_SRET(MemoryInteractor mi, uint256 mmIndex, uint32 insn, uint64 pc) 
-  public returns (bool) {
-    uint64 priv = mi.read_iflags_PRV(mmIndex);
-    uint64 mstatus = mi.read_mstatus(mmIndex);
-
-    if (priv < RiscVConstants.PRV_S() || (priv == RiscVConstants.PRV_S() && (mstatus & RiscVConstants.MSTATUS_TSR_MASK() != 0))) {
-      return false;
-    } else {
-      uint64 spp = (mstatus & RiscVConstants.MSTATUS_SPP_MASK()) >> RiscVConstants.MSTATUS_SPP_SHIFT();
-      // Set the IE state to previous IE state
-      uint64 spie = (mstatus & RiscVConstants.MSTATUS_SPIE_MASK()) >> RiscVConstants.MSTATUS_SPIE_SHIFT();
-      mstatus = (mstatus & ~RiscVConstants.MSTATUS_SIE_MASK()) | (spie << RiscVConstants.MSTATUS_SIE_SHIFT());
-
-      // set SPIE to 1
-      mstatus |= RiscVConstants.MSTATUS_SPIE_MASK();
-      // set SPP to U
-      mstatus &= ~RiscVConstants.MSTATUS_SPP_MASK();
-      mi.write_mstatus(mmIndex, mstatus);
-      if(priv != spp){
-        mi.set_priv(mmIndex, spp);
-      }
-      mi.write_pc(mmIndex, mi.read_sepc(mmIndex));
-      return true;
+    function executeECALL(
+        MemoryInteractor mi,
+        uint256 mmIndex,
+        uint32 insn,
+        uint64 pc
+    ) public
+    {
+        uint64 priv = mi.readIflagsPRV(mmIndex);
+        uint64 mtval = mi.readMtval(mmIndex);
+        // TO-DO: Are parameter valuation order deterministic? If so, we dont need to allocate memory
+        Exceptions.raiseException(
+            mi,
+            mmIndex,
+            Exceptions.MCAUSE_ECALL_BASE() + priv,
+            mtval
+        );
     }
-  }
 
-  function execute_MRET(MemoryInteractor mi, uint256 mmIndex, uint32 insn, uint64 pc)
-  public returns(bool) {
-    uint64 priv = mi.read_iflags_PRV(mmIndex);
-
-    if (priv < RiscVConstants.PRV_M()) {
-      return false;
-    } else {
-      uint64 mstatus = mi.read_mstatus(mmIndex);
-      uint64 mpp = (mstatus & RiscVConstants.MSTATUS_MPP_MASK()) >> RiscVConstants.MSTATUS_MPP_SHIFT();
-      // set IE state to previous IE state
-      uint64 mpie = (mstatus & RiscVConstants.MSTATUS_MPIE_MASK()) >> RiscVConstants.MSTATUS_MPIE_SHIFT();
-      mstatus = (mstatus & ~RiscVConstants.MSTATUS_MIE_MASK()) | (mpie << RiscVConstants.MSTATUS_MIE_SHIFT());
-
-      // set MPIE to 1
-      mstatus |= RiscVConstants.MSTATUS_MPIE_MASK();
-      // set MPP to U
-      mstatus &= ~RiscVConstants.MSTATUS_MPP_MASK();
-      mi.write_mstatus(mmIndex, mstatus);
-
-      if (priv != mpp){
-        mi.set_priv(mmIndex, mpp);
-      }
-      mi.write_pc(mmIndex, mi.read_mepc(mmIndex));
-      return true;
+    function executeEBREAK(
+        MemoryInteractor mi,
+        uint256 mmIndex,
+        uint32 insn,
+        uint64 pc
+    ) public
+    {
+        Exceptions.raiseException(
+            mi,
+            mmIndex,
+            Exceptions.MCAUSE_BREAKPOINT(),
+            mi.readMtval(mmIndex)
+        );
     }
-  }
 
-  function execute_WFI(MemoryInteractor mi, uint256 mmIndex, uint32 insn, uint64 pc)
-  public returns(bool) {
-    uint64 priv = mi.read_iflags_PRV(mmIndex);
-    uint64 mstatus = mi.read_mstatus(mmIndex);
+    function executeSRET(
+        MemoryInteractor mi,
+        uint256 mmIndex,
+        uint32 insn,
+        uint64 pc
+    )
+    public returns (bool)
+    {
+        uint64 priv = mi.readIflags_PRV(mmIndex);
+        uint64 mstatus = mi.readMstatus(mmIndex);
 
-    if (priv == RiscVConstants.PRV_U() || (priv == RiscVConstants.PRV_S() && (mstatus & RiscVConstants.MSTATUS_TW_MASK() != 0))) {
-      return false;
-    } else {
-      uint64 mip = mi.read_mip(mmIndex);
-      uint64 mie = mi.read_mie(mmIndex);
-      // Go to power down if no enable interrupts are pending
-      if ((mip & mie) == 0) {
-        mi.set_iflags_I(mmIndex, true);
-      }
-      return true;
+        if (priv < RiscVConstants.PRV_S() || (priv == RiscVConstants.PRV_S() && (mstatus & RiscVConstants.MSTATUS_TSR_MASK() != 0))) {
+            return false;
+        } else {
+            uint64 spp = (mstatus & RiscVConstants.MSTATUS_SPP_MASK()) >> RiscVConstants.MSTATUS_SPP_SHIFT();
+            // Set the IE state to previous IE state
+            uint64 spie = (mstatus & RiscVConstants.MSTATUS_SPIE_MASK()) >> RiscVConstants.MSTATUS_SPIE_SHIFT();
+            mstatus = (mstatus & ~RiscVConstants.MSTATUS_SIE_MASK()) | (spie << RiscVConstants.MSTATUS_SIE_SHIFT());
+
+            // set SPIE to 1
+            mstatus |= RiscVConstants.MSTATUS_SPIE_MASK();
+            // set SPP to U
+            mstatus &= ~RiscVConstants.MSTATUS_SPP_MASK();
+            mi.writeMstatus(mmIndex, mstatus);
+            if (priv != spp) {
+                mi.setPriv(mmIndex, spp);
+            }
+            mi.writePc(mmIndex, mi.readSepc(mmIndex));
+            return true;
+        }
     }
-  }
 
+    function executeMRET(
+        MemoryInteractor mi,
+        uint256 mmIndex,
+        uint32 insn,
+        uint64 pc
+    )
+    public returns(bool)
+    {
+        uint64 priv = mi.readIflags_PRV(mmIndex);
+
+        if (priv < RiscVConstants.PRV_M()) {
+            return false;
+        } else {
+            uint64 mstatus = mi.readMstatus(mmIndex);
+            uint64 mpp = (mstatus & RiscVConstants.MSTATUS_MPP_MASK()) >> RiscVConstants.MSTATUS_MPP_SHIFT();
+            // set IE state to previous IE state
+            uint64 mpie = (mstatus & RiscVConstants.MSTATUS_MPIE_MASK()) >> RiscVConstants.MSTATUS_MPIE_SHIFT();
+            mstatus = (mstatus & ~RiscVConstants.MSTATUS_MIE_MASK()) | (mpie << RiscVConstants.MSTATUS_MIE_SHIFT());
+
+            // set MPIE to 1
+            mstatus |= RiscVConstants.MSTATUS_MPIE_MASK();
+            // set MPP to U
+            mstatus &= ~RiscVConstants.MSTATUS_MPP_MASK();
+            mi.writeMstatus(mmIndex, mstatus);
+
+            if (priv != mpp) {
+                mi.setPriv(mmIndex, mpp);
+            }
+            mi.writePc(mmIndex, mi.readMepc(mmIndex));
+            return true;
+        }
+    }
+
+    function executeWFI(
+        MemoryInteractor mi,
+        uint256 mmIndex,
+        uint32 insn,
+        uint64 pc
+    )
+    public returns(bool)
+    {
+        uint64 priv = mi.readIflags_PRV(mmIndex);
+        uint64 mstatus = mi.readMstatus(mmIndex);
+
+        if (priv == RiscVConstants.PRV_U() || (priv == RiscVConstants.PRV_S() && (mstatus & RiscVConstants.MSTATUS_TW_MASK() != 0))) {
+            return false;
+        } else {
+            uint64 mip = mi.readMip(mmIndex);
+            uint64 mie = mi.readMie(mmIndex);
+            // Go to power down if no enable interrupts are pending
+            if ((mip & mie) == 0) {
+                mi.setIflags_I(mmIndex, true);
+            }
+            return true;
+        }
+    }
 }
