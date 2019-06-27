eeaa5cbe43f91aa8cb9c385cc4e43a04cf83cfc3
felipeargento <felipeargento@gmail.com>
2019-06-27 20:43:46 +0800

    Removed ShadowAddresses depedency
    Also changed parameters for Fetch and Execute functions, they no longer
    receive the address of mi, the receive mi itself.

--- contracts/Step.sol
+++ contracts/Step.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.5.0;
 
 //Libraries
-import "./ShadowAddresses.sol";
 import "./RiscVConstants.sol";
 import "./RiscVDecoder.sol";
 import "../contracts/MemoryInteractor.sol";
@@ -11,9 +10,7 @@ import {Execute} from "../contracts/Execute.sol";
 import {Interrupts} from "../contracts/Interrupts.sol";
 
 
-//TO-DO: use instantiator pattern so we can always use same instance of mm/pc etc
 contract Step {
-    // event Print(string message, uint value);
     event StepGiven(uint8 exitCode);
 
     MemoryInteractor mi;
@@ -27,27 +24,27 @@ contract Step {
         // If machine is halted - nothing else to do. H flag is stored on the least
         // signficant bit on iflags register.
         // Reference: The Core of Cartesi, v1.02 - figure 1.
-        uint64 iflags = mi.memoryRead(mmIndex, ShadowAddresses.getIflags());
-        //emit Print("iflags", uint(iflags));
+        uint64 iflags = mi.readIflags(mmIndex);
+
         if ((iflags & 1) != 0) {
             //machine is halted
             return endStep(mmIndex, 0);
         }
         //Raise the highest priority interrupt
-        Interrupts.raiseInterruptIfAny(mmIndex, address(mi));
+        Interrupts.raiseInterruptIfAny(mmIndex, mi);
 
         //Fetch Instruction
         Fetch.fetchStatus fetchStatus;
         uint64 pc;
         uint32 insn;
 
-        (fetchStatus, insn, pc) = Fetch.fetchInsn(mmIndex, address(mi));
+        (fetchStatus, insn, pc) = Fetch.fetchInsn(mmIndex, mi);
 
         if (fetchStatus == Fetch.fetchStatus.success) {
             // If fetch was successfull, tries to execute instruction
             if (Execute.executeInsn(
                     mmIndex,
-                    address(mi),
+                    mi,
                     insn,
                     pc
                 ) == Execute.executeStatus.retired
@@ -55,17 +52,16 @@ contract Step {
                 // If executeInsn finishes successfully we need to update the number of
                 // retired instructions. This number is stored on minstret CSR.
                 // Reference: riscv-priv-spec-1.10.pdf - Table 2.5, page 12.
-                uint64 minstret = mi.memoryRead(mmIndex, ShadowAddresses.getMinstret());
-                //emit Print("minstret", uint(minstret));
-                mi.memoryWrite(mmIndex, ShadowAddresses.getMinstret(), minstret + 1);
+                uint64 minstret = mi.readMinstret(mmIndex);
+                mi.writeMinstret(mmIndex, minstret + 1);
             }
         }
         // Last thing that has to be done in a step is to update the cycle counter.
         // The cycle counter is stored on mcycle CSR.
         // Reference: riscv-priv-spec-1.10.pdf - Table 2.5, page 12.
-        uint64 mcycle = mi.memoryRead(mmIndex, ShadowAddresses.getMcycle());
-        //emit Print("mcycle", uint(mcycle));
-        mi.memoryWrite(mmIndex, ShadowAddresses.getMcycle(), mcycle + 1);
+        uint64 mcycle = mi.readMcycle(mmIndex);
+        mi.writeMcycle(mmIndex, mcycle + 1);
+
         return endStep(mmIndex, 0);
     }
 
