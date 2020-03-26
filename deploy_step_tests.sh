86b5f1f0701e2b40930ee6f6768ccf02ee11a084
Stephen Chen <stephen@cartesi.io>
2020-03-26 23:43:34 +0800

    Add deploy scripts for step/ram tests

---
+++ deploy_step_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# this script will compile and migrate the contracts using truffle
+
+# remove build directory to do a clean build
+rm ./build/ -rf
+cd node_modules/@cartesi/util && truffle migrate --network development && cd ../../../
+cd node_modules/@cartesi/arbitration && truffle migrate --network development && cd ../../../
+truffle migrate --network development
