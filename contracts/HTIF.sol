25bce6c3f5a026396a7200b422c7cf3f841a293c
Gabriel Barros <gabriel.barros@cartesi.io>
2020-06-10 11:32:18 -0300

    Add constants referenced at c++ implementation

--- contracts/HTIF.sol
+++ contracts/HTIF.sol
@@ -27,6 +27,18 @@ library HTIF {
     uint64 constant HTIF_TOHOST_ADDR_CONST = 0x40008000;
     uint64 constant HTIF_FROMHOST_ADDR_CONST = 0x40008008;
 
+    // [c++] enum HTIF_devices
+    uint64 constant HTIF_DEVICE_HALT = 0;        ///< Used to halt machine
+    uint64 constant HTIF_DEVICE_CONSOLE = 1;     ///< Used for console input and output
+    uint64 constant HTIF_DEVICE_YIELD = 2;       ///< Used to yield control back to host
+
+    // [c++] enum HTIF_commands
+    uint64 constant HTIF_HALT_HALT = 0; 
+    uint64 constant HTIF_CONSOLE_GETCHAR = 0; 
+    uint64 constant HTIF_CONSOLE_PUTCHAR = 1; 
+    uint64 constant HTIF_YIELD_PROGRESS = 0; 
+    uint64 constant HTIF_YIELD_ROLLUP = 1; 
+
     /// @notice reads htif
     /// @param mi Memory Interactor with which Step function is interacting.
     /// @param mmIndex Index corresponding to the instance of Memory Manager
@@ -104,19 +116,19 @@ library HTIF {
 
         mi.writeHtifTohost(mmIndex, tohost);
 
-        if (device == 0) {
+        if (device == HTIF_DEVICE_HALT) {
             return htifHalt(
                 mi,
                 mmIndex,
                 cmd,
                 payload);
-        } else if (device == 1) {
+        } else if (device == HTIF_DEVICE_CONSOLE) {
             return htifConsole(
                 mi,
                 mmIndex,
                 cmd,
                 payload);
-        } else if (device == 2) {
+        } else if (device == HTIF_DEVICE_YIELD) {
             return htifYield(
                 mi,
                 mmIndex,
@@ -134,7 +146,7 @@ library HTIF {
         uint64 payload)
     internal returns (bool)
     {
-        if (cmd == 0 && ((payload & 1) == 1) ) {
+        if (cmd == HTIF_HALT_HALT && ((payload & 1) == 1) ) {
             //set iflags to halted
             mi.setIflagsH(mmIndex, true);
         }
@@ -150,7 +162,7 @@ library HTIF {
     {
         // TO-DO: check if h is yieldable?
         mi.setIflagsY(mmIndex, true);
-        mi.writeHtifFromhost(mmIndex, (uint64(2) << 56) | cmd << 48);
+        mi.writeHtifFromhost(mmIndex, (HTIF_DEVICE_YIELD << 56) | cmd << 48);
         return true;
     }
 
@@ -161,9 +173,9 @@ library HTIF {
         uint64 payload)
     internal returns (bool)
     {
-        if (cmd == 1) {
+        if (cmd == HTIF_CONSOLE_PUTCHAR) {
             htifPutchar(mi, mmIndex);
-        } else if (cmd == 0) {
+        } else if (cmd == HTIF_CONSOLE_GETCHAR) {
             htifGetchar(mi, mmIndex);
         } else {
             // Unknown HTIF console commands are silently ignored
@@ -175,14 +187,21 @@ library HTIF {
     returns (bool)
     {
         // TO-DO: what to do in the blockchain? Generate event?
-        mi.writeHtifFromhost(mmIndex, (uint64(1) << 56) | uint64(0) << 48);
+
+        // @NOTICE: C++ version diverge
+        // a->write_htif_fromhost(((uint64_t)HTIF_DEVICE_CONSOLE << 56) | ((uint64_t)1 << 48));
+        mi.writeHtifFromhost(mmIndex, (HTIF_DEVICE_CONSOLE << 56) | uint64(0) << 48);
         return true;
     }
 
     function htifGetchar(MemoryInteractor mi, uint256 mmIndex) internal
     returns (bool)
     {
-        mi.writeHtifFromhost(mmIndex, (uint64(1) << 56) | uint64(1) << 48);
+        // @NOTICE: C++ version possibly diverge 
+        // int c = h? h->console_get_char(): 0;
+        // a->write_htif_fromhost(((uint64_t)HTIF_DEVICE_CONSOLE << 56) |
+        // ((uint64_t)HTIF_CONSOLE_GETCHAR << 48) | c);
+        mi.writeHtifFromhost(mmIndex, (HTIF_DEVICE_CONSOLE << 56) | uint64(1) << 48);
         return true;
     }
 
