147af028a193dea0592ab17a4e612a85a821b00b
Milton Jonathan <milton.jonathan@cartesi.io>
2020-09-18 18:11:29 -0300

    Fixing TestRamMMInstantiator for Solidity 0.7
    - We can no longer override MMInstantiator methods that do not have
    the "virtual" keyword specified
    - Solution here is to copy the original code from MMInstantiator,
    keeping the changes implemented in this contract.

--- contracts/TestRamMMInstantiator.sol
+++ contracts/TestRamMMInstantiator.sol
@@ -10,24 +10,23 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-
-
 pragma solidity ^0.7.0;
 
-import "@cartesi/arbitration/contracts/MMInstantiator.sol";
+import "@cartesi/util/contracts/InstantiatorImpl.sol";
+import "@cartesi/util/contracts/Decorated.sol";
+import "@cartesi/util/contracts/Merkle.sol";
+import "@cartesi/arbitration/contracts/MMInterface.sol";
 import "@cartesi/util/contracts/BitsManipulationLibrary.sol";
-
 
 /// @title Test RAM MM Instantiator
 /// @author Stephen Chen
 /// @notice A mock memory manager for running test_ram.py
 /// @dev This should never be deployed to Main net.
 /// @dev This contract is unsafe.
-contract TestRamMMInstantiator is MMInstantiator {
-  // the provider will fill the memory for the client to read and write
-  // memory starts with hash and all values that are inserted are first verified
-  // then client can read inserted values and write some more
-  // finally the provider has to update the hash to account for writes
+contract TestRamMMInstantiator is InstantiatorImpl, MMInterface, Decorated {
+    //
+    // TESTRAM-SPECIFIC CODE
+    //
 
     struct MemoryMap {
         mapping(uint64 => bytes8) map;
@@ -36,12 +35,17 @@ contract TestRamMMInstantiator is MMInstantiator {
     event HTIFExit(uint256 _index, uint64 _exitCode, bool _halt);
 
     /// @notice Stop memory insertion and start read and write phase
-    function finishProofPhase(uint256 _index) public override
+    function finishProofPhase(uint256 _index)
+        public
+        override
         onlyInstantiated(_index)
         //onlyBy(instance[_index].provider)
         increasesNonce(_index)
     {
-        require(instance[_index].currentState == state.WaitingProofs, "CurrentState is not WaitingProofs, cannot finishProofPhase");
+        require(
+            instance[_index].currentState == state.WaitingProofs,
+            "CurrentState is not WaitingProofs, cannot finishProofPhase"
+        );
         instance[_index].currentState = state.WaitingReplay;
         emit FinishedProofs(_index);
     }
@@ -49,47 +53,60 @@ contract TestRamMMInstantiator is MMInstantiator {
     /// @notice Replays a read in memory that has been proved to be correct
     /// according to initial hash
     /// @param _position of the desired memory
-    function read(uint256 _index, uint64 _position) public override
+    function read(uint256 _index, uint64 _position)
+        public
+        override
         onlyInstantiated(_index)
         //onlyBy(instance[_index].client)
         increasesNonce(_index)
         returns (bytes8)
     {
-        require(instance[_index].currentState == state.WaitingReplay, "CurrentState is not WaitingReplay, cannot read");
+        require(
+            instance[_index].currentState == state.WaitingReplay,
+            "CurrentState is not WaitingReplay, cannot read"
+        );
         require((_position & 7) == 0, "Position is not aligned");
         bytes8 value = ramInstance[_index].map[_position];
-        emit ValueRead(
-            _index,
-            _position,
-            value);
+        emit ValueRead(_index, _position, value);
         return value;
     }
 
     /// @notice Replays a write in memory that was proved correct
     /// @param _position of the write
     /// @param _value to be written
-    function write(uint256 _index, uint64 _position, bytes8 _value) public override
+    function write(
+        uint256 _index,
+        uint64 _position,
+        bytes8 _value
+    )
+        public
+        override
         //onlyBy(instance[_index].client)
         increasesNonce(_index)
         onlyInstantiated(_index)
     {
-        require(instance[_index].currentState == state.WaitingReplay, "CurrentState is not WaitingReplay, cannot write");
+        require(
+            instance[_index].currentState == state.WaitingReplay,
+            "CurrentState is not WaitingReplay, cannot write"
+        );
         require((_position & 7) == 0, "Position is not aligned");
-        bytes8 oldValue = ramInstance[_index].map[_position];
+        // bytes8 oldValue = ramInstance[_index].map[_position];
         ramInstance[_index].map[_position] = _value;
-        emit ValueWritten(
-            _index,
-            _position,
-            _value);
+        emit ValueWritten(_index, _position, _value);
     }
 
     /// @notice Stop write (or read) phase
-    function finishReplayPhase(uint256 _index) public override
+    function finishReplayPhase(uint256 _index)
+        public
+        override
         onlyInstantiated(_index)
         //onlyBy(instance[_index].client)
         increasesNonce(_index)
     {
-        require(instance[_index].currentState == state.WaitingReplay, "CurrentState is not WaitingReplay, cannot finishReplayPhase");
+        require(
+            instance[_index].currentState == state.WaitingReplay,
+            "CurrentState is not WaitingReplay, cannot finishReplayPhase"
+        );
         //require(instance[_index].historyPointer == instance[_index].history.length, "History pointer does not match length");
         //delete(instance[_index].history);
         //delete(instance[_index].historyPointer);
@@ -100,7 +117,8 @@ contract TestRamMMInstantiator is MMInstantiator {
     }
 
     /// @notice Perform a read in HTIF to get the arbitrary exit code
-    function htifExit(uint256 _index) public
+    function htifExit(uint256 _index)
+        public
         onlyInstantiated(_index)
         returns (uint64)
     {
@@ -110,11 +128,356 @@ contract TestRamMMInstantiator is MMInstantiator {
         val = BitsManipulationLibrary.uint64SwapEndian(val);
 
         uint64 bit0 = val & 1;
-        uint64 payload = val << 16 >> 17;
+        uint64 payload = (val << 16) >> 17;
 
         halt = (bit0 == 1);
 
         emit HTIFExit(_index, payload, halt);
         return payload;
+    }
+
+    //
+    // UNMODIFIED MMINSTANTIATOR CODE
+    //
+    // the provider will fill the memory for the client to read and write
+    // memory starts with hash and all values that are inserted are first verified
+    // then client can read inserted values and write some more
+    // finally the provider has to update the hash to account for writes
+
+    struct ReadWrite {
+        bool wasRead;
+        uint64 position;
+        bytes8 value;
+    }
+
+    // IMPLEMENT GARBAGE COLLECTOR AFTER AN INSTACE IS FINISHED!
+    struct MMCtx {
+        address provider;
+        address client;
+        bytes32 initialHash;
+        bytes32 newHash; // hash after some write operations have been proved
+        ReadWrite[] history;
+        uint256 historyPointer;
+        state currentState;
+    }
+
+    mapping(uint256 => MMCtx) internal instance;
+
+    // These are the possible states and transitions of the contract.
+    //
+    // +---+
+    // |   |
+    // +---+
+    //   |
+    //   | instantiate
+    //   v
+    // +---------------+    | proveRead
+    // | WaitingProofs |----| proveWrite
+    // +---------------+
+    //   |
+    //   | finishProofPhase
+    //   v
+    // +----------------+    |read
+    // | WaitingReplay  |----|write
+    // +----------------+
+    //   |
+    //   | finishReplayPhase
+    //   v
+    // +----------------+
+    // | FinishedReplay |
+    // +----------------+
+    //
+
+    event MemoryCreated(uint256 _index, bytes32 _initialHash);
+    event ValueProved(
+        uint256 _index,
+        bool _wasRead,
+        uint64 _position,
+        bytes8 _value
+    );
+    event ValueRead(uint256 _index, uint64 _position, bytes8 _value);
+    event ValueWritten(uint256 _index, uint64 _position, bytes8 _value);
+    event FinishedProofs(uint256 _index);
+    event FinishedReplay(uint256 _index);
+
+    /// @notice Instantiate a memory manager instance.
+    /// @param _provider address that will provide memory values/proofs.
+    /// @param _client address that will consume memory values/proofs.
+    /// @param _initialHash hash before divergence, in which both client and provider agree.
+    /// @return MemoryManager index.
+    function instantiate(
+        address _provider,
+        address _client,
+        bytes32 _initialHash
+    ) public override returns (uint256) {
+        require(_provider != _client, "Provider and client need to differ");
+        MMCtx storage currentInstance = instance[currentIndex];
+        currentInstance.provider = _provider;
+        currentInstance.client = _client;
+        currentInstance.initialHash = _initialHash;
+        currentInstance.newHash = _initialHash;
+        currentInstance.historyPointer = 0;
+        currentInstance.currentState = state.WaitingProofs;
+        emit MemoryCreated(currentIndex, _initialHash);
+
+        active[currentIndex] = true;
+        return currentIndex++;
+    }
+
+    /// @notice Proves that a certain value in current memory is correct
+    // @param _position The address of the value to be confirmed
+    // @param _value The value in that address to be confirmed
+    // @param proof The proof that this value is correct
+    function proveRead(
+        uint256 _index,
+        uint64 _position,
+        bytes8 _value,
+        bytes32[] memory proof
+    )
+        public
+        onlyInstantiated(_index)
+        onlyBy(instance[_index].provider)
+        increasesNonce(_index)
+    {
+        require(
+            instance[_index].currentState == state.WaitingProofs,
+            "CurrentState is not WaitingProofs, cannot proveRead"
+        );
+        require(
+            Merkle.getRoot(_position, _value, proof) ==
+                instance[_index].newHash,
+            "Merkle proof does not match"
+        );
+        instance[_index].history.push(ReadWrite(true, _position, _value));
+        emit ValueProved(_index, true, _position, _value);
+    }
+
+    /// @notice Register a write operation and update newHash
+    /// @param _position to be written
+    /// @param _oldValue before write
+    /// @param _newValue to be written
+    /// @param proof The proof that the old value was correct
+    function proveWrite(
+        uint256 _index,
+        uint64 _position,
+        bytes8 _oldValue,
+        bytes8 _newValue,
+        bytes32[] memory proof
+    )
+        public
+        onlyInstantiated(_index)
+        onlyBy(instance[_index].provider)
+        increasesNonce(_index)
+    {
+        require(
+            instance[_index].currentState == state.WaitingProofs,
+            "CurrentState is not WaitingProofs, cannot proveWrite"
+        );
+        // check proof of old value
+        require(
+            Merkle.getRoot(_position, _oldValue, proof) ==
+                instance[_index].newHash,
+            "Merkle proof of write does not match"
+        );
+        // update root
+        instance[_index].newHash = Merkle.getRoot(_position, _newValue, proof);
+        instance[_index].history.push(ReadWrite(false, _position, _newValue));
+        emit ValueProved(_index, false, _position, _newValue);
+    }
+
+    // getter methods
+    function isConcerned(uint256 _index, address _user)
+        public
+        override
+        view
+        returns (bool)
+    {
+        return ((instance[_index].provider == _user) ||
+            (instance[_index].client == _user));
+    }
+
+    function getState(uint256 _index, address)
+        public
+        view
+        onlyInstantiated(_index)
+        returns (
+            address _provider,
+            address _client,
+            bytes32 _initialHash,
+            bytes32 _newHash,
+            uint256 _numberSubmitted,
+            bytes32 _currentState
+        )
+    {
+        MMCtx memory i = instance[_index];
+
+        return (
+            i.provider,
+            i.client,
+            i.initialHash,
+            i.newHash,
+            i.history.length,
+            getCurrentState(_index)
+        );
+    }
+
+    function getSubInstances(uint256, address)
+        public
+        override
+        pure
+        returns (address[] memory, uint256[] memory)
+    {
+        address[] memory a = new address[](0);
+        uint256[] memory i = new uint256[](0);
+        return (a, i);
+    }
+
+    function provider(uint256 _index)
+        public
+        view
+        onlyInstantiated(_index)
+        returns (address)
+    {
+        return instance[_index].provider;
+    }
+
+    function client(uint256 _index)
+        public
+        view
+        onlyInstantiated(_index)
+        returns (address)
+    {
+        return instance[_index].client;
+    }
+
+    function initialHash(uint256 _index)
+        public
+        view
+        onlyInstantiated(_index)
+        returns (bytes32)
+    {
+        return instance[_index].initialHash;
+    }
+
+    function newHash(uint256 _index)
+        public
+        override
+        view
+        onlyInstantiated(_index)
+        returns (bytes32)
+    {
+        return instance[_index].newHash;
+    }
+
+    // state getters
+
+    function getCurrentState(uint256 _index)
+        public
+        override
+        view
+        onlyInstantiated(_index)
+        returns (bytes32)
+    {
+        if (instance[_index].currentState == state.WaitingProofs) {
+            return "WaitingProofs";
+        }
+        if (instance[_index].currentState == state.WaitingReplay) {
+            return "WaitingReplay";
+        }
+        if (instance[_index].currentState == state.FinishedReplay) {
+            return "FinishedReplay";
+        }
+        require(false, "Unrecognized state");
+    }
+
+    /// @notice Get the worst case scenario duration for a specific state
+    /// @param _roundDuration security parameter, the max time an agent
+    //          has to react and submit one simple transaction
+    /// @param _timeToStartMachine time to build the machine for the first time
+    function getMaxStateDuration(
+        state _state,
+        uint256 _roundDuration,
+        uint256 _timeToStartMachine
+    ) private pure returns (uint256) {
+        if (_state == state.WaitingProofs) {
+            // proving siblings is assumed to be free
+            // so its time to start the machine
+            // + one round duration to send the proofs
+            // + one transaction for finishProofPhase transaction
+            return _timeToStartMachine + uint256(2) * _roundDuration;
+        }
+        if (_state == state.WaitingReplay) {
+            // one transaction for the step function to be completed
+            return _roundDuration;
+        }
+        if (_state == state.FinishedReplay) {
+            // one transaction for finishReplay transaction
+            return _roundDuration;
+        }
+
+        require(false, "Unrecognized state");
+    }
+
+    /// @notice Get the worst case scenario duration for an instance of this contract
+    /// @param _roundDuration security parameter, the max time an agent
+    //          has to react and submit one simple transaction
+    /// @param _timeToStartMachine time to build the machine for the first time
+    function getMaxInstanceDuration(
+        uint256 _roundDuration,
+        uint256 _timeToStartMachine
+    ) public override pure returns (uint256) {
+        uint256 waitingProofsDuration = getMaxStateDuration(
+            state.WaitingProofs,
+            _roundDuration,
+            _timeToStartMachine
+        );
+
+        uint256 waitingReplayDuration = getMaxStateDuration(
+            state.WaitingReplay,
+            _roundDuration,
+            _timeToStartMachine
+        );
+
+        uint256 finishProofsDuration = getMaxStateDuration(
+            state.WaitingProofs,
+            _roundDuration,
+            _timeToStartMachine
+        );
+
+        return
+            waitingProofsDuration +
+            waitingReplayDuration +
+            finishProofsDuration;
+    }
+
+    // remove these functions and change tests accordingly
+    function stateIsWaitingProofs(uint256 _index)
+        public
+        override
+        view
+        onlyInstantiated(_index)
+        returns (bool)
+    {
+        return instance[_index].currentState == state.WaitingProofs;
+    }
+
+    function stateIsWaitingReplay(uint256 _index)
+        public
+        override
+        view
+        onlyInstantiated(_index)
+        returns (bool)
+    {
+        return instance[_index].currentState == state.WaitingReplay;
+    }
+
+    function stateIsFinishedReplay(uint256 _index)
+        public
+        override
+        view
+        onlyInstantiated(_index)
+        returns (bool)
+    {
+        return instance[_index].currentState == state.FinishedReplay;
     }
 }
