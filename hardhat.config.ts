fb2c34dc3f6507a9889b380720c1941104446bf8
Danilo Tuler <danilo@cartesi.io>
2022-02-01 14:16:04 -0500

    Deploy contracts to all testnets

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -37,11 +37,7 @@ const config: HardhatUserConfig = {
         rinkeby: infuraNetwork("rinkeby", 4, 6283185),
         kovan: infuraNetwork("kovan", 42, 6283185),
         goerli: infuraNetwork("goerli", 5, 6283185),
-        matic_testnet: {
-            url: "https://rpc-mumbai.matic.today",
-            chainId: 80001,
-            accounts: mnemonic ? { mnemonic } : undefined,
-        },
+        matic_testnet: infuraNetwork("polygon-mumbai", 80001),
         bsc_testnet: {
             url: "https://data-seed-prebsc-1-s1.binance.org:8545",
             chainId: 97,
