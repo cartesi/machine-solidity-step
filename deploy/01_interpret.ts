c7d5f87097802ed6dec774583d4b1a671e2f4cac
Stephen Chen <stephen.chen@cartesi.io>
2023-03-29 22:37:15 +0800

    feat: add uarch deploy script

---
+++ deploy/01_interpret.ts
@@ -0,0 +1,50 @@
+// Copyright 2023 Cartesi Pte. Ltd.
+
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction, DeployOptions } from "hardhat-deploy/types";
+
+const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+    const { deployments, getNamedAccounts, network } = hre;
+    const { deploy } = deployments;
+    const { deployer } = await getNamedAccounts();
+    const { BitsManipulation } = await deployments.all();
+
+    const deterministicDeployment = network.name !== "iotex_testnet";
+
+    const opts: DeployOptions = {
+        deterministicDeployment,
+        from: deployer,
+        log: true,
+    }
+
+    const MemoryAccessLog = await deploy("MemoryAccessLog", {
+        ...opts,
+        libraries: {
+            ["BitsManipulation"]: BitsManipulation.address,
+        },
+    });
+
+    const UArchState = await deploy("UArchState", {
+        ...opts,
+        libraries: {
+            ["MemoryAccessLog"]: MemoryAccessLog.address,
+        },
+    });
+
+    await deploy("UArchInterpret", {
+        ...opts
+    });
+};
+
+func.tags = ["UArch"];
+func.dependencies = ["BitsManipulation"];
+export default func;
