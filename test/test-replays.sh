8a019d83f9daab5d9ea47289392d8bf279159143
Carsten Munk <carsten@zippie.com>
2023-06-27 11:36:25 +0200

    feat: add replay tests
    can re-generate using test/gen-replays.sh
    
    Signed-off-by: Carsten Munk <carsten@zippie.com>

---
+++ test/test-replays.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+for i in test/uarch-log/rv64ui*; do
+	BASE=`basename $i .json | sed "s/-/_/g"`
+	cp test/UArchReplay.t.template test/UArchReplay_$BASE.t.sol
+	sed -i "s/@X@/$BASE/g" test/UArchReplay_$BASE.t.sol
+	P=`basename $i`
+	sed -i "s/@PATH@/$P/g" test/UArchReplay_$BASE.t.sol
+done
+
+for i in test/uarch-log/rv64ui*; do
+	BASE=`basename $i .json | sed "s/-/_/g"`
+	forge test -vvv --match-contract UArchReplay_${BASE}_Test
+	if [ $? != 0 ]; then
+		exit 1
+	fi
+done
