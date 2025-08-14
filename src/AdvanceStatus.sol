870cff1a6f53936cdf5665314d6d384fc0eccdf7
Stephen Chen <20940639+stephenctw@users.noreply.github.com>
2025-08-14 23:54:55 +0800

    feat: add `AdvanceStatus.sol`

---
+++ src/AdvanceStatus.sol
@@ -0,0 +1,64 @@
+// Copyright Cartesi and individual authors (see AUTHORS)
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+/// @title AdvanceStatus
+/// @notice Return advance status
+
+pragma solidity ^0.8.0;
+
+import "./EmulatorCompat.sol";
+import "./EmulatorConstants.sol";
+
+enum Status {
+    NOT_YIELDED,
+    ACCEPTED,
+    REJECTED,
+    EXCEPTION
+}
+
+library AdvanceStatus {
+    // START OF AUTO-GENERATED CODE
+
+    function advanceStatus(AccessLogs.Context memory a)
+        internal
+        pure
+        returns (Status)
+    {
+        if (!EmulatorCompat.readIflagsY(a)) {
+            return Status.NOT_YIELDED;
+        }
+
+        uint64 tohost =
+            EmulatorCompat.readWord(a, EmulatorConstants.HTIF_TOHOST_ADDRESS);
+        uint16 reason = uint16((tohost >> 16) & ((1 << 16) - 1));
+
+        if (reason == EmulatorConstants.CMIO_YIELD_MANUAL_REASON_RX_ACCEPTED) {
+            return Status.ACCEPTED;
+        } else if (
+            reason == EmulatorConstants.CMIO_YIELD_MANUAL_REASON_RX_REJECTED
+        ) {
+            return Status.REJECTED;
+        } else if (
+            reason == EmulatorConstants.CMIO_YIELD_MANUAL_REASON_TX_EXCEPTION
+        ) {
+            return Status.EXCEPTION;
+        } else {
+            revert("Invalid reason");
+        }
+    }
+
+    // END OF AUTO-GENERATED CODE
+}
